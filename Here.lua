4767f4bbd5a37a7857a0034678ad67f3dfa069c9
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 18:20:03 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -12,5 +12,6 @@ local tab = win:Tab("Sam Question", "http://www.roblox.com/asset/?id=6023426915"
 tab:Button("Dupe", "Working", function()
     workspace:WaitForChild("UserData"):WaitForChild("User_"..game.Players.LocalPlayer.UserId):WaitForChild("UpdateClothing_Extras"):FireServer("A","\255",31)
 tab:Button("Claim Compass", "Claim 10 Compass", function()
+            end)
     workspace.Merchants.QuestMerchant.Clickable.Retum:FireServer("Claim10")
 end)
