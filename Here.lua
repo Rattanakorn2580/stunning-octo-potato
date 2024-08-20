ebc41290af2ad6ad983f8557fe321bcfaac0bb81
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 14:12:46 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -100,9 +100,9 @@ v.Character.Head:FindFirstChild("ESP") then
                 local BillboardGui = 
 Instance.new("BillboardGui") 
                 local TextLabel = 
-Instance.new("TextLabel") BillboardGui.Parent = v.Character.Head 
-                BillboardGui.ZIndexBehavior = 
-Enum.ZIndexBehavior.Sibling 
+Instance.new("TextLabel") 
+                BillboardGui.Parent = v.Character.Head 
+                BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling 
                 BillboardGui.Active = true 
                 BillboardGui.Name = "ESP" 
                 BillboardGui.AlwaysOnTop = true 
@@ -110,8 +110,10 @@ Enum.ZIndexBehavior.Sibling
                 BillboardGui.Size = UDim2.new(0, 200, 0, 50) 
                 BillboardGui.StudsOffset = Vector3.new(0, 2.5, 0) 
                 TextLabel.Parent = BillboardGui 
-                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255) TextLabel.BackgroundTransparency = 1.000 
-                TextLabel.Size = UDim2.new(0, 200, 0, 50) TextLabel.Font = Enum.Font.GothamBold 
+                TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255) 
+                TextLabel.BackgroundTransparency = 1.000 
+                TextLabel.Size = UDim2.new(0, 200, 0, 50) 
+                TextLabel.Font = Enum.Font.GothamBold 
                 TextLabel.Text = v.Name 
                 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255) 
                 TextLabel.TextScaled = true 
