4bbfca15b88ec351292ae7c6563f362c7929c7e1
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 17:38:05 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,8 +1,20 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
+-- TAP AUTO FARM
+local tab = win:Tab("AutoFarm")
+
+-- TAP AUTO HAKI
+local tab = win:Tab("Auto Haki")
+
+-- TAP TP ISLAND
+local tab = win:Tab("Tp island")
+
 -- TAP PLAYER
-local tab = win:Tab("Player", "Player")
+local tab = win:Tab("Player")
 tab:Button("Dupe", "Working", function()
     workspace:WaitForChild("UserData"):WaitForChild("User_"..game.Players.LocalPlayer.UserId):WaitForChild("UpdateClothing_Extras"):FireServer("A","\255",31)
 end)
+
+-- TAP SAM
+local tab = win:Tab("Auto Sam")
