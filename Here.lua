7f764eebd490c56556d03419047c9fe00d7d14e3
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 16:55:02 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,2 +1,2 @@
-local Library = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
