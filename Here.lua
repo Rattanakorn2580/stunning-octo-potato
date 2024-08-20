5ae37ddb5c349e5c56ed2f6d8d3b47b1a0d6c131
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 16:52:23 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,3 +1,2 @@
 local Library = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local Window = Library.CreateLib("OPL Hub", "DarkTheme")
-local CloseBind = Enum.KeyCode.RightControl
