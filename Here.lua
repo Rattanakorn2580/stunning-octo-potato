c6ae3fd29ec1be0b3e8a93c4e1e4c4e50622d5a3
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 10:50:57 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -2,13 +2,38 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 
 local Window = Library.CreateLib("Here Hub", "DarkTheme")
 
-local Tab = Window:NewTab("Teleport")
+local Tab = Window:NewTab("Island TP")
 local Section = Tab:NewSection("Tp island")
+
+-- TP
 
 Section:NewButton("KaiIsland", "tp to kaiisland", function()
     game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
 end)
 
--- TP
 Section:NewButton("Crab island", "tp to crab island", function()    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("Pirate island", "tp to pirate island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("Chiff island", "tp to chiff island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("Purple island", "tp to purple island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("fail island", "tp to fail island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("Pyramid island", "tp to pyramid island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
+end)
+
+Section:NewButton("Kizma island", "tp to krizma island", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
 end)
