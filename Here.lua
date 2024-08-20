2bb348291620c30f246f1eb879bfee231c4b8411
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 18:33:13 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -7,6 +7,10 @@ tab:Button("Kai Island", "Tp to Kai Island", function()
     game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
 end)
 
+tab:Button("Dupe", "Working", function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1066, 282.5, 1624.5, 1, 0, 0, 0, 1, 0, 0, 0, 1))
+    end)
+
 -- TAB SAM
 local tab = win:Tab("Sam Question", "http://www.roblox.com/asset/?id=6023426915")
 tab:Button("Dupe", "Working", function()
