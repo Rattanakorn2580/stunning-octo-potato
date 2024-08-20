0f011ecd7eba251d5d113ddac56292739252d354
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 16:34:48 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,5 +1,5 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")() 
-local win = Flux:Window("PREVIEW", "Baseplate", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local win = Flux:Window("HANDSOME HUB", "OPL:Anachy", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
 -- UI Tab 1
 local Tap = win:tap("Tap 1,")
