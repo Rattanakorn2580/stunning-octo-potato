99ffd194c804ad7ac5973442a4b5d9e423291bec
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 17:04:20 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,2 +1,11 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+
+-- TAP AUTO FARM
+local Tap = Window:Tap("Auto Farm")
+tap:Button("Auto Farm", "function is not work" function()
+    print("Clicken")
+tap:Label("Auto Farm")
+tap:Line()
+tap:Toggle("Auto Click", "function not work now" function()
+        
