dee6583c7652e7eabc8cd21ff0d73deea6b4663d
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 17:53:32 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -2,31 +2,31 @@ local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scr
 local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
 -- TAP AUTO FARM
-local tab = win:Tab("Auto Farm")
+local tab = win:Tab("Auto Farm", "Auto Farm")
 tab:Button("Auto Farm", "not Working", function()
     print("")
 end)
 
 -- TAP AUTO HAKI
-local tab = win:Tab("Haki Farm")
+local tab = win:Tab("Haki Farm, "Haki Farm")
 tab:Button("Auto Haki", "not Working", function()
     print("")
 end)
 
 -- TAP TP ISLAND
-local tab = win:Tab("TP island")
+local tab = win:Tab("TP island", "TP island")
 tab:Button("Kai Island", "Tp to Kai Island", function()
     game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1240.9720458984375, 412.9999084472656, 11083.3154296875))
 end)
 
 -- TAP PLAYER
-local tab = win:Tab("Player")
+local tab = win:Tab("Player", "Player")
 tab:Button("View Player", "not Working", function()
     print("")
 end)
 
 -- TAP SAM
-local tab = win:Tab("Sam Question")
+local tab = win:Tab("Sam Question", "Sam Question")
 tab:Button("Auto Sam", "Not Working", function()
     print("")
 end)
