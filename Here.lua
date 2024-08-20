1b1bda143e752d489c61eb3a4c31547ff89b0836
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 17:26:48 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,7 +1,7 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
--- TAP AUTO FARM
+-- TAP PLAYER
 local tab = win:Tab("Player", "http://www.roblox.com/asset/?id=6023426915")
 tab:Button("Dupe", "Working", function()
     workspace:WaitForChild("UserData"):WaitForChild("User_"..game.Players.LocalPlayer.UserId):WaitForChild("UpdateClothing_Extras"):FireServer("A","\255",31)
