1d3d5ecddb4dc122ab425fcfd3e2ee9d608cc76c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 16:50:04 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,10 +1,3 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("OPL Hub", "DarkTheme")
-
-local PresetColor = Color3.fromRGB(66, 134, 255)
-local UserInputService = game:GetService("UserInputService") 
-local TweenService = game:GetService("TweenService") 
-local RunService = game:GetService("RunService") 
-local LocalPlayer = game:GetService("Players").LocalPlayer 
-local Mouse = LocalPlayer:GetMouse() 
 local CloseBind = Enum.KeyCode.RightControl
