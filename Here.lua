ef6b24953fa7cbd43bf37778e977dcadeada389a
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-20 16:54:23 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,2 +1,2 @@
 local Library = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
-local Window = Library.CreateLib("OPL Hub", "DarkTheme")
+local win = Flux:Window("OPL Hub", "Test Script", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
