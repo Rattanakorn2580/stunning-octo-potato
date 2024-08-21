0a87dbafcc44616861c23748c2c20bcb5df58bbd
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-21 19:30:50 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -9,3 +9,16 @@ end
 local drop = Section:NewDropdown("Select Player!", "Click To Select", Plr, function(t)
    PlayerTP = t
 end)
+Section:NewButton("Click To TP", "", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
+end)
+Section:NewToggle("Auto Tp", "", function(t)
+_G.TPPlayer = t
+while _G.TPPlayer do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
+end
+end)
+
+Section:NewButton("Refresh Dropdown","Refresh Dropdown", function()
+  drop:Refresh(Plr)
+end)
