cd7195c59a8ecd4cf21d89bbd605d95468b5d525
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-21 20:19:46 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -1,19 +1,24 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("NOOB HUB", "DarkTheme")
 
+-- AUTO FARM
 local Tab = Window:NewTab("Auto Farm") 
 local Section = Tab:NewSection("Auto Equip") 
 local Weaponlist = {} 
-local Weapon = nil 
-for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do 
+local Weapon = nil
+
+for i,v in 
+pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do 
     table.insert(Weaponlist,v.Name) 
 end 
 Section:NewDropdown("select weapon", " ", Weaponlist, function(currentOption) 
         Weapon = currentOption 
-    end) 
+    end)
+
 Section:NewToggle("Auto Equip", " ", function(a) 
         AutoEquiped = a 
-    end) 
+    end)
+
 spawn(function() 
 while wait() do 
 if AutoEquiped then 
@@ -23,6 +28,8 @@ end)
 end 
 end 
 end)
+
+-- PLAYER
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("Select Player!")
 Plr = {}
