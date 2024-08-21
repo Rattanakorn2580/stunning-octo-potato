2f22952d288f6c8cb87bacb49255a68b5afa0f98
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-21 22:27:05 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("NOOB HUB", "DarkTheme")
+local Window = Library.CreateLib("NOOB HUB", "DarkTheme", Enum.KeyCode.LeftControl)
 
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")                       
