8e83949dfe03faf67f000eec93592ac98f0f5972
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-21 19:03:05 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,6 +1,11 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local win = Flux:Window("Here Hub", "!! อยู่ในช่วงพัฒนา !!", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
+local weaponlist = {}
+local weapon = nil
+for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+    table.insert(Weaponlist,v.Name)
+end)
 -- TAB NEW UPDATE
 local tab = win:Tab("UPDATE", "http://www.roblox.com/asset/?id=6023426915")
 tab:Button("No Update", "...", function()
@@ -15,8 +20,9 @@ tab:Toggle("Auto Click", "Auto Clicker", function()
     Flux:Notification("Coming Soon..", "Alright")
     end)
 
-tab:Dropdown("Select Weapon", {""}, Weaponlist, function()
-    end)
+tab:Dropdown("Select Weapon", {""}, Weaponlist, function(currentoption)
+    weapon = currentoption
+end)
 tab:Toggle("Auto Equip", "", function()
 end)
        
