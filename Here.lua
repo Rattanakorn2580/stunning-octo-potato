d2fef3a14f9675345f3cefe10b128b2ffe400ac0
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 22:25:26 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -9,4 +9,18 @@ _G.Tp = t
 while _G.Tp do wait()
 game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-949, 213.5, 1655, 1, 0, 0, 0, 1, 0, 0, 0, 1))
 end
-end)
+end
+_G.Skill = true
+while _G.Skill do
+    wait()
+FireSwingRemote()
+for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+            if v:IsA("Tool") and v:FindFirstChild("abilitySlot") then
+        if v.abilitySlot.Value == "q" then
+            v:FindFirstChildWhichIsA("RemoteEvent"):FireServer()
+            v.cooldown.Changed:Connect(function()      
+            v.cooldown.Value = 0
+            end)
+        end
+        end
+        end)
