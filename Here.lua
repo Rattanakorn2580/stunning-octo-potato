7a0ebae39d5b0e29bc4e47c531f072dc37f79712
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 06:37:26 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,13 +4,15 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
-Section:NewToggle("Auto Farm", " ", function(t)
-    _G.EnemiesHit = t
-    for i,v in 
+
+for i,v in 
 pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
       v.HumanoidRootPart.Size = Vector3.new(35, 35, 35) 
       v.HumanoidRootPart.Transparency = 0.5 
-        end
+end
+Section:NewToggle("Auto Farm", " ", function(t)
+    _G.EnemiesHit = t
+    end
         
 spawn(function()
 while _G.EnemieHit wait() do
