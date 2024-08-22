767d171bfafdd3664716d9f3708af8e027e1e229
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 22:34:32 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,5 +4,3 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Magma")
-Section:NewToggle("Auto Equip", " ", function()
-    end)
