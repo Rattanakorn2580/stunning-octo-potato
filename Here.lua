2bc48d550b90a289758f851a6fdd11a9ddb70ccd
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 19:44:06 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -5,12 +5,10 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 local Tab = Window:NewTab("Auto Find")
 local Section = Tab:NewSection("Auto Find")
 
-local Compass = nil
 for i,v in pairs (game:GetService("Workspace").Compass.Handle.Bit:GetDescendants()) 
-if v.Name "Compass" then
+if v.Name "TochInterest" then
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.v.Parent.CFrame
-wait(.1)
  end
 Section:NewToggle("Auto Find", " ", function(a)
     AutoFinding = a
