489bfd587955fc0837e277e25a1333e2417411ff
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 21:41:57 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,9 +1,20 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
--- AUTO FIND
-local Tab = Window:NewTab("Auto Find")
-local Section = Tab:NewSection("Auto Find")
+-- AUTO FARM
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Auto Magma")
 Section:NewToggle("Auto Equip", " ", function(a) 
-        AutoEquiped = a
+        AutoFarm = a 
+    end)
+
+spawn(function() 
+while wait() do 
+if AutoFarm then 
+pcall(function() 
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-949, 213.5, 1655, 1, 0, 0, 0, 1, 0, 0, 0, 1)) 
+end) 
+end 
+end 
+end)
   end)
