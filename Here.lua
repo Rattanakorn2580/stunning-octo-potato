e516a2814553f459b73278b2c70729262dd2d550
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 20:25:56 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -2,8 +2,8 @@ local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
 -- AUTO FIND
-local Tab = Window:NewTab("Player")
-local Section = Tab:NewSection("Select Player!")
+local Tab = Window:NewTab("Auto Find")
+local Section = Tab:NewSection("Auto Find")
 Section:NewButton("Refresh Dropdown","Refresh Dropdown", function()
   drop:Refresh(Plr)
 end)
