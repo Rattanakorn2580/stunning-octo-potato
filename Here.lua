3a82b6d6321a153ff15752fc04b4719869d4ce7f
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 19:38:39 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,1 +1,27 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("Test X", "DarkTheme")
 
+-- AUTO FIND
+local Tab = Window:NewTab("Auto Find")
+local Section = Tab:NewSection("Auto Find")
+
+for i,v in pairs (game:GetService("Workspace").Compass.Handle.Bit:GetDescendants()) 
+if v.Name "Compass" then
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.v.Parent.CFrame
+wait(.1)
+ end
+Section:NewToggle("Auto Find", " ", function(a)
+    AutoFinding = a
+  end)
+
+spawn(function()
+while wait() do
+if AutoFinding then
+pcall(function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.v.Parent.CFrame
+end)
+end
+end
+end)
+    
