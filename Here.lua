4d735d6074a85548061ba20c6bb611edb4ef6af6
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 22:30:10 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,13 +4,8 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Magma")
-Section:NewToggle("Auto Equip", " ", function(t) 
-_G.Tp = t
-while _G.Tp do wait()
-game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-949, 213.5, 1655, 1, 0, 0, 0, 1, 0, 0, 0, 1))
-end
-end
-_G.Skill = true
+Section:NewToggle("Auto Equip", " ", function(t)
+_G.Skill = t
 while _G.Skill do
     wait()
 FireSwingRemote()
