963808889f11ac60b42e459b17a8d954a8d44f95
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 22:31:42 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,18 +4,5 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Magma")
-Section:NewToggle("Auto Equip", " ", function(t)
-_G.Skill = t
-while _G.Skill do
-    wait()
-FireSwingRemote()
-for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-            if v:IsA("Tool") and v:FindFirstChild("abilitySlot") then
-        if v.abilitySlot.Value == "q" then
-            v:FindFirstChildWhichIsA("RemoteEvent"):FireServer()
-            v.cooldown.Changed:Connect(function()      
-            v.cooldown.Value = 0
-            end)
-        end
-        end
-        end)
+Section:NewToggle("Auto Equip", " ", function()
+    end)
