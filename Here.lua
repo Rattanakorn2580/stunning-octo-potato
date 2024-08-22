5c39a4f46159ef833af5264c5c7aa2abfb0ce55a
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 22:37:50 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,6 +1,8 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
--- AUTO FARM
-local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Magma")
+-- PLAYER
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Player")
+Section:NewToggle("God Mode", " ", function()
+  end)
