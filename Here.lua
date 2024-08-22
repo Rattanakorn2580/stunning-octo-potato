62da5e8f4e9462a09d474aca5f6773114bef13f9
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 20:23:56 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -2,8 +2,8 @@ local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
 -- AUTO FIND
-local Tab = Window:NewTab("Auto Find")
-local Section = Tab:NewSection("Auto Find")
-Section:NewToggle("ESP","", function(a)
-	BringCp = a
-	end)
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Select Player!")
+Section:NewToggle("Auto Equip", " ", function(a) 
+        AutoEquiped = a 
+    end)
