c9e0bc13c05692b0369cf7e120cf6eb7cd3c053c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 06:26:37 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -2,9 +2,9 @@ local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
 -- PLAYER
-local Tab = Window:NewTab("Player")
-local Section = Tab:NewSection("Player")
-Section:NewToggle("God Mode", " ", function()
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Auto Farm")
+Section:NewToggle("Auto Farm", " ", function()
     for i,v in 
 pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
       v.HumanoidRootPart.Size = Vector3.new(35, 35, 35) 
