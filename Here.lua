e63428df78aeffc65b2e79e36fb487f155d3a58c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 06:46:39 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,6 +1,5 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
--- AUTO FARM
-local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Farm")
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Select Player!")
