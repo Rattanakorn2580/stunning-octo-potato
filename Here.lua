c89d9b13d99984e11a248a35e46132d70a80cea2
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-22 20:13:29 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,3 +4,19 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 -- AUTO FIND
 local Tab = Window:NewTab("Auto Find")
 local Section = Tab:NewSection("Auto Find")
+Section:NewButton("ESP","", function()
+local plr=game:GetService'Players'.LocalPlayer
+	local dftofind='Compass'
+	while true do
+	local char=plr.Character or plr.CharacterAdded:Wait()
+	local HRP=char:WaitForChild'HumanoidRootPart'
+	for i,v in next, workspace:GetChildren() do
+	local Handle=v:FindFirstChild'Handle'
+	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then
+	Handle.CFrame=HRP.CFrame
+	end
+	end
+	wait(1)
+	end
+ 
+end)
