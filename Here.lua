af9400bc08359e62d5d8b0530e62913f0b1228da
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 22:07:03 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -5,29 +5,3 @@ local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
 Section:NewButton("Auto Farm","Refresh Dropdown", function()
     end)
-_G.bringmob = true
-while _G.bringmob do wait()
-    pcall(function()
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-for x,y in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-if v.Name == "Lv9 Bandit" then
-    if y.Name == "Lv9 Bandit" then
-   v.HumanoidRootPart.CFrame = y.HumanoidRootPart.CFrame
-   v.HumanoidRootPart.Size = Vector3.new(60,60,60)
-   y.HumanoidRootPart.Size = Vector3.new(60,60,60)
-   v.HumanoidRootPart.Transparency = 1
-   v.HumanoidRootPart.CanCollide = false
-   y.HumanoidRootPart.CanCollide = false
-   v.Humanoid.WalkSpeed = 0
-   y.Humanoid.WalkSpeed = 0
-   v.Humanoid.JumpPower = 0
-   y.Humanoid.JumpPower = 0
-   if sethiddenproperty then
-     sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
-end
-end
-end
-end
-end
-end)
-end
