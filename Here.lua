d50bf629876a0c9908ff09c76fe37ba89dcdd76d
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 10:50:08 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -3,3 +3,7 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("Select Player!")
+Section:NewToggle("Bring Fruit", " ", function(t) 
+_G.Fruittype = t
+    {"Light Fruit","Rumble Fruit","Magma Fruit","Quake Fruit","Phoenix Fruit","Vampire Fruit","Dark Fruit","Smoke Fruit","Hollow Fruit","Snow Fruit","Venom Fruit","Ope Fruit","Gas Fruit","Sand Fruit","Flare Fruit","Candy Fruit","Plasma Fruit","Gravity Fruit","Chilly Fruit"}
+  end)
