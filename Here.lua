57b18155bd6029ab91df7aac7c2c58257e9f9a32
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 10:57:20 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -3,7 +3,19 @@ local Window = Library.CreateLib("Test X", "DarkTheme")
 
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("Select Player!")
+
+local Fruittype = {}
 Section:NewToggle("Bring Fruit", " ", function(t) 
 _G.Fruittype = t
-    {"Light Fruit","Rumble Fruit","Magma Fruit","Quake Fruit","Phoenix Fruit","Vampire Fruit","Dark Fruit","Smoke Fruit","Hollow Fruit","Snow Fruit","Venom Fruit","Ope Fruit","Gas Fruit","Sand Fruit","Flare Fruit","Candy Fruit","Plasma Fruit","Gravity Fruit","Chilly Fruit"}
-  end)
+        {"Light Fruit","Rumble Fruit","Magma Fruit","Quake Fruit","Phoenix Fruit","Vampire Fruit","Dark Fruit","Smoke Fruit","Hollow Fruit","Snow Fruit","Venom Fruit","Ope Fruit","Gas Fruit","Sand Fruit","Flare Fruit","Candy Fruit","Plasma Fruit","Gravity Fruit","Chilly Fruit"}
+    end
+
+spawn(function() 
+while wait() do 
+if _G.Fruittype then 
+pcall(function() 
+workspace.Trees.Handle.Clickable.Retum:FireServer()
+end) 
+end 
+end 
+end)
