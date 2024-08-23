f89c3af713429f92a289ce7489c4591f671a7909
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 11:03:56 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -7,14 +7,4 @@ local Section = Tab:NewSection("Select Player!")
 Section:NewToggle("Bring Fruit", " ", function(t) 
 _G.Fruittype = t
         {"Light Fruit","Rumble Fruit","Magma Fruit","Quake Fruit","Phoenix Fruit","Vampire Fruit","Dark Fruit","Smoke Fruit","Hollow Fruit","Snow Fruit","Venom Fruit","Ope Fruit","Gas Fruit","Sand Fruit","Flare Fruit","Candy Fruit","Plasma Fruit","Gravity Fruit","Chilly Fruit"}
-    end
-
-spawn(function() 
-while wait() do 
-if _G.Fruittype then 
-pcall(function() 
-workspace.Trees.Handle.Clickable.Retum:FireServer()
-end) 
-end 
-end 
-end)
+        end)
