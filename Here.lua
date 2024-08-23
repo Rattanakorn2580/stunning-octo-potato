811ba4e4ac77402babbb182cf713bc19e8c5ebe4
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-23 23:10:19 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,7 +1,98 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Test X", "DarkTheme")
 
-local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Farm")
-Section:NewButton("Auto Farm","Refresh Dropdown", function()
-    end)
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Select Player!")
+Plr = {}
+for i,v in pairs(game:GetService("Players"):GetChildren()) do
+    table.insert(Plr,v.Name) 
+end
+local drop = Section:NewDropdown("Select Player!", "Click To Select", Plr, function(t)
+   PlayerTP = t
+end)
+Section:NewButton("Click To TP", "", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
+end)
+Section:NewToggle("Auto Tp", "", function()
+    local Camera = workspace.CurrentCamera 
+local Players = game:GetService("Players") 
+local RunService = game:GetService("RunService") 
+local UserInputService = game:GetService("UserInputService") 
+local TweenService = game:GetService("TweenService") 
+local LocalPlayer = Players.LocalPlayer 
+local Holding = false 
+
+_G.AimbotEnabled = true 
+_G.TeamCheck = false -- If set to true then the script would only lock your aim at enemy team members. 
+_G.AimPart = "Head" -- Where the aimbot script would lock at. 
+_G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart. 
+_G.CircleSides = 64 -- How many sides the FOV circle would have. 
+_G.CircleColor = Color3.fromRGB(255, 255, 255) -- (RGB) Color that the FOV circle would appear as. 
+_G.CircleTransparency = 0.7 -- Transparency of the circle. 
+_G.CircleRadius = 80 -- The radius of the circle / FOV. 
+_G.CircleFilled = false -- Determines whether or not the circle is filled. 
+_G.CircleVisible = true -- Determines whether or not the circle is visible. 
+_G.CircleThickness = 0 -- The thickness of the circle. 
+
+local FOVCircle = Drawing.new("Circle") 
+FOVCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2) 
+FOVCircle.Radius = _G.CircleRadius 
+FOVCircle.Filled = _G.CircleFilled 
+FOVCircle.Color = _G.CircleColor 
+FOVCircle.Visible = _G.CircleVisible 
+FOVCircle.Radius = _G.CircleRadius 
+FOVCircle.Transparency = _G.CircleTransparency 
+FOVCircle.NumSides = _G.CircleSides 
+FOVCircle.Thickness = _G.CircleThickness 
+
+local function GetClosestPlayer() 
+local MaximumDistance = _G.CircleRadius 
+local Target = nil 
+
+for _, v in next, Players:GetPlayers() do 
+if v.Name ~= LocalPlayer.Name then 
+if _G.TeamCheck == true then 
+if v.Team ~= LocalPlayer.Team then 
+if v.Character ~= nil then 
+if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then 
+if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then 
+
+local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position) 
+local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude 
+if VectorDistance < MaximumDistance then 
+Target = v 
+end 
+end 
+end 
+end 
+end 
+
+else 
+if v.Character ~= nil then 
+if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then 
+if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then 
+
+local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position) 
+local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude 
+if VectorDistance < MaximumDistance then 
+
+Target = v 
+
+end 
+end 
+end 
+end 
+end 
+end 
+end  
+
+return Target 
+end  
+
+UserInputService.InputBegan:Connect(function(Input) 
+if Input.UserInputType == Enum.UserInputType.MouseButton2 then 
+
+Holding = true 
+end 
+end)
+        
