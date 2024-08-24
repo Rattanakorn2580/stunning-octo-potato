8c03cf2890268b7ab00a1be1a652489ae93f1673
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-24 12:58:04 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -3,8 +3,22 @@ local Window = Library.CreateLib("Opl X", "DarkTheme")
 
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Magu")
-Section:NewToggle("Auto Equip", " ", function(a) 
-        AutoEquiped = a 
+local Section = Tab:NewSection("Auto Farm")
+Section:NewToggle("Auto Magma", " ", function(a) 
+        AutoMagma = a 
     end)
+
+spawn(function()
+while wait() do
+if AutoMagma then
+pcall(function()
+local A_1 = 899012.68
+local A_2 = "MagmaPower1"
+local A_3 = "StopCharging"
+local A_4 = CFrame.new(-1040.10706, 215, 1659.80774, 0.615341127, 0.553823352, -0.560923517, 1.4901163e-08, 0.711596251, 0.702588737, 0.788261056, -0.432331741, 0.437874347)
+local A_5 = game:GetService("Workspace").IslandCaver.Stones.Stone
+local A_6 = 100
+local Event = game:GetService("Workspace")["ICU_XxWhiteknightxX"].Powers.Magma.RemoteEvent
+Event:FireServer(A_1, A_2, A_3, A_4, A_5, A_6)
+                                        end)
 
