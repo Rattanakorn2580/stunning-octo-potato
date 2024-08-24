663cb0164246a2651efec96303d475590ea2478b
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-24 12:52:14 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -1,27 +1,10 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Winnable Hub", "DarkTheme")
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("Opl X", "DarkTheme")
 
+-- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Felix | ComeBxck")
-MOBS = {}
- 
-for i,v in pairs(game:GetService("Workspace").Enemies.Mob:GetChildren()) do
-    table.insert(MOBS,v.Name)
-end
-Section:NewToggle("Auto Farm", "", function(a)
-    _G.AutoFarm = a
-    while _G.AutoFarm do wait()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies.Mob[Select].HumanoidRootPart.CFrame
-end
-end)
- 
-Section:NewDropdown("Please Select Monster", "", MOBS, function(currentOption)
-    Select = currentOption
-end)
- 
-Section:NewButton("Refresh", "", function()
-    table.clear(MOBS)
-for i,v in pairs(game:GetService("Workspace").Enemies.Mob:GetChildren()) do
-    table.insert(MOBS,v.Name)
-end
-end)
+local Section = Tab:NewSection("Auto Magu")
+Section:NewToggle("Auto Equip", " ", function(a) 
+        AutoEquiped = a 
+    end)
+
