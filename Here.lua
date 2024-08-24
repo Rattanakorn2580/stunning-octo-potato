ec7843fe4c117bd8e3377b05f1ae43203bdb1ee2
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 06:43:06 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -8,6 +8,7 @@ local Section = Tab:NewSection("Auto Farm")
 Section:NewToggle("Auto-Farm", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait()
+            if AngryBob then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 end
  end)
