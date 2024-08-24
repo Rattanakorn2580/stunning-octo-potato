9332660bd58d022f0ac9a93299ef29414f1fd054
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-24 23:45:28 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -4,17 +4,27 @@ local Window = Library.CreateLib("Opl X", "DarkTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
-Section:NewToggle("Auto Magma", " ", function()
-    end)
 
-while wait() do
-local A_1 = 899012.68
-local A_2 = "MagmaPower1"
-local A_3 = "StopCharging"
-local A_4 = CFrame.new(-1040.10706, 215, 1659.80774, 0.615341127, 0.553823352, -0.560923517, 1.4901163e-08, 0.711596251, 0.702588737, 0.788261056, -0.432331741, 0.437874347)
-local A_5 = game:GetService("Workspace").IslandCaver.Stones.Stone
-local A_6 = 100
-local Event = game:GetService("Workspace")["ICU_XxWhiteknightxX"].Powers.Magma.RemoteEvent
-Event:FireServer(A_1, A_2, A_3, A_4, A_5, A_6)
+MONS = {}
+ 
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+    table.insert(MONS,v.Name)
 end
+ 
+Section:NewToggle("Auto-Farm", "", function(state)
+    _G.AutoFarm = state
+    while _G.AutoFarm do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv40 Cave Demon"].HumanoidRootPart.CFrame
+end
+end)
 
+Section:NewDropdown("Please Select Monster", "", MONS, function(currentOption)
+    Select = currentOption
+end)
+ 
+Section:NewButton("Refresh", "", function()
+    table.clear(MONS)
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+    table.insert(MONS,v.Name)
+end
+end)
