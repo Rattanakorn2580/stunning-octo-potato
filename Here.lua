8f9e36dbfd223c778154cd11dbf400c9d31ace1a
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 06:29:50 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -14,17 +14,7 @@ end
 Section:NewToggle("Auto-Farm", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[Select].HumanoidRootPart.CFrame
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 end
 end)
 
-Section:NewDropdown("Select Monster", "", MONS, function(currentOption)
-    Select = currentOption
-end)
- 
-Section:NewButton("Refresh", "", function()
-    table.clear(MONS)
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-    table.insert(MONS,v.Name)
-  end
-end)
