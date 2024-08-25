91f084a8119533ec15a12c1c3fc80301e3d17e2f
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 07:07:27 +0700

    Update Here.lua

--- Here.lua
+++ Here.lua
@@ -5,9 +5,16 @@ local Window = Library.CreateLib("Opl X", "DarkTheme")
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
 
-Section:NewToggle("Auto-Farm", "", function(state)
+Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
+end
+    end)
+
+Section:NewToggle("Lv1 Crab", "", function(state)
+    _G.AutoFarm = state
+    while _G.AutoFarm do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv1 Crab"].HumanoidRootPart.CFrame
 end
     end)
