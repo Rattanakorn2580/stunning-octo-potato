e71d9a31a7b934152626a917f6ffd8f5f0a7733d
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:29:54 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -6,20 +6,15 @@ local Window = Library.CreateLib("List Mob", "BloodTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Select Auto Mob Farm")
-
-if game:GetService("Players").LocalPlayer.Character.Services:FindFirstChild("Client") then
-    game:GetService("Players").LocalPlayer.Character.Services["Client"].Disabled = true
-end
 MONS = {}
  
 for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
     table.insert(MONS,v.Name)
 end
-
 Section:NewToggle("Auto Farm", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[Select].HumanoidRootPart.CFrame
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Select"].HumanoidRootPart.CFrame
 end
     end)
 
