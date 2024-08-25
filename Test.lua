100601aa37889d98a501bb60dd822d6cb0c59771
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:33:39 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -8,11 +8,11 @@ Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(1)
             for i,v in pairs(game:GetService("Workspace").Enemies:GetDescendants()) do
-        if "Lv2 Angry Bob" then
+        if v.Name = "Lv2 Angry Bob" then
         if v.Humanoid.Health >= 0 then
     repeat task.wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
-until _G.AutoFarm false or v.Humanoid.Health <= 0 then
+until v.Humanoid.Health <= 0 then
 end
 end
 end
