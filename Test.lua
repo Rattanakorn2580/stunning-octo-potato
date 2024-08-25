abdac9c1c4bda345c67ebd3aff5408b1e76e63ee
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:26:07 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -6,20 +6,30 @@ local Window = Library.CreateLib("List Mob", "BloodTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Select Auto Mob Farm")
--- BYPASS TP
+
 if game:GetService("Players").LocalPlayer.Character.Services:FindFirstChild("Client") then
     game:GetService("Players").LocalPlayer.Character.Services["Client"].Disabled = true
 end
-
 MONS = {}
  
 for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
     table.insert(MONS,v.Name)
 end
 
-Section:NewToggle("Lv2 Angry Bob", "", function(state)
+Section:NewToggle("Auto Farm", "", function(state)
     _G.AutoFarm = state
-    while _G.AutoFarm do wait(2.5)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
+    while _G.AutoFarm do wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[Select].HumanoidRootPart.CFrame
 end
     end)
+
+Section:NewDropdown("Select Enemies", "", MONS, function(currentOption)
+    Select = currentOption
+end)
+ 
+Section:NewButton("Refresh", "", function()
+    table.clear(MONS)
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+    table.insert(MONS,v.Name)
+end
+end)
