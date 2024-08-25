382542e8b08f4c9c06bd01216f6a88ef5f9ed956
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:48:55 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -8,6 +8,6 @@ local Section = Tab:NewSection("Select Auto Mob Farm")
 Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(2.5)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame(0,-5,0)
 end
     end)
