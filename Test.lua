2afad1c6d26bc211f7e20f743703b9e8ebe62bdd
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:08:16 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -13,7 +13,22 @@ Section:NewToggle("Auto Farm", "", function(state)
   if v.Name == "Lv2 Angry Bob" then
   if Humanoid.Health >= 0 then
   repeat task.wait()
-  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.Health
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+        until _G.AutoFarm false or v.Humanoid.Health <= 0 then
+       end
+      end
+     end)
+   end
+
+Section:NewToggle("Lv1 Crab", "", function(state)
+  _G.AutoFarm = state
+ while _G.AutoFarm do wait(1)
+  pcall(function()
+  for i,v in pairs(game:GetService("Workspace").Enemies:Descendants()) do
+  if v.Name == "Lv1 Crab" then
+  if Humanoid.Health >= 0 then
+  repeat task.wait()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
         until _G.AutoFarm false or v.Humanoid.Health <= 0 then
        end
       end
