0f4f9a3da2393106cd3984f737b2ec82ef2c4445
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:03:18 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,14 +4,14 @@ if game:GetService("Players").LocalPlayer.Character.Services:FindFirstChild("Cli
 end
 
 
-local mobs = {} 
-getgenv().mob = nil 
+local enemies = {} 
+getgenv().enemies = nil 
 
 -- MOBS
 for _,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
     insert = true 
-    for _,v2 in pairs(mobs) do if v2 == v.Name then insert = false end end 
-    if insert then table.insert(mobs, v.Name) end 
+    for _,v2 in pairs(enemies) do if v2 == v.Name then insert = false end end 
+    if insert then table.insert(enemies, v.Name) end 
 end
 
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
@@ -23,51 +23,51 @@ local Section = Tab:NewSection("Select Auto Mob Farm")
 
 -- auto farm
 local Main = Window:NewTab("Main")
-local MobFarmSection = Main:NewSection("Mob Farm")
+local MobFarmSection = NewSection("Mob Farm")
 
-local mobdropdown = MobFarmSection:NewDropdown("Choose Mob", "Chooses the mob to autofarm", mobs, function(v)
-    getgenv().mob = v
+local dropdown = Section:NewDropdown("Choose Mob", "Chooses the mob to autofarm", enemies, function(v)
+    getgenv().enemies = v
 end)
 
-MobFarmSection:NewToggle("Start Mob Farm", "Toggles the autofarming of the mobs", function(v)
-    getgenv().autofarmmobs = v
+Section:NewToggle("Start Mob Farm", "Toggles the autofarming of the enemies", function(v)
+    getgenv().autofarmenemies = v
     while wait() do
-        if getgenv().autofarmmobs == false then return end 
-        if getgenv().mob == nil then 
+        if getgenv().autofarmenemies == false then return end 
+        if getgenv().enemies == nil then 
             game.StarterGui:SetCore("SendNotification", {
                 Title = "!! FAIL !!", 
-                Text = "Please choose your MOBS",
+                Text = "Please choose your ENEMIES",
                 Icon = "",
                 Duration = 2.5
             })
-            getgenv().autofarmmobs = false
+            getgenv().autofarmenemies = false
             return
         end
-        local mob = game:GetService("Workspace").Enemies:FindFirstChild(getgenv().mob)
+        local mob = game:GetService("Workspace").Enemies:FindFirstChild(getgenv().enemies)
         if mob == nil then
             game.StarterGui:SetCore("SendNotification", { 
                 Title = "Info!",
-                Text = "There is currently no spawned mobs of this type!\nJust wait until they spawn", 
+                Text = "There is currently no spawned enemies of this type!\nJust wait until they spawn", 
                 Icon = "", 
                 Duration = 2.5
             })
             while wait() do 
                 wait() 
-                if getgenv().autofarmmobs == false then return end 
-                if game:GetService("Workspace").Enemies:FindFirstChild(getgenv().mob) ~= nil then break; end
+                if getgenv().autofarmenemies == false then return end 
+                if game:GetService("Workspace").Enemies:FindFirstChild(getgenv().enemies) ~= nil then break; end
             end 
         else
-            local mob2 = mob
+            local enemies2 = enemie
             while wait() do
-                mob = game:GetService("Workspace").Enemies:FindFirstChild(getgenv().mob)
-                if mob ~= mob2 then break; end
-                if getgenv().autofarmmobs == false then return end
-                if mob ~= nil then
-                    if mob:FindFirstChild("Humanoid") then
-                        if mob.Humanoid.Health == 0 then wait(0.1) mob:Destroy() break; end 
+                mob = game:GetService("Workspace").Enemies:FindFirstChild(getgenv().enemies)
+                if enemies ~= enemies2 then break; end
+                if getgenv().autofarmenemies == false then return end
+                if enemies ~= nil then
+                    if enemies:FindFirstChild("Humanoid") then
+                        if enemies.Humanoid.Health == 0 then wait(0.1) enemies:Destroy() break; end 
                     end
-                    if mob:FindFirstChild("HumanoidRootPart") then
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame 
+                    if enemies:FindFirstChild("HumanoidRootPart") then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = enemies.HumanoidRootPart.CFrame 
                     end
                 end
                 wait() 
@@ -83,19 +83,19 @@ game:GetService("Workspace").Enemies.ChildAdded:Connect(function()
     
     for _,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
         insert = true 
-        for _,v2 in pairs(mobs) do if v2 == v.Name then insert = false end end
-        if insert then table.insert(mobs, v.Name) end 
+        for _,v2 in pairs(enemies) do if v2 == v.Name then insert = false end end
+        if insert then table.insert(enemies, v.Name) end 
     end
-    mobdropdown:Refresh(mobs)
+    dropdown:Refresh(enemies)
 end)
 
 game:GetService("Workspace").Enemies.ChildRemoved:Connect(function() 
-    for _,v2 in pairs(mobs) do table.remove(mobs, _) end 
+    for _,v2 in pairs(enemies) do table.remove(enemies, _) end 
     
     for _,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
         insert = true 
-        for _,v2 in pairs(mobs) do if v2 == v.Name then insert = false end end 
-        if insert then table.insert(mobs, v.Name) end 
+        for _,v2 in pairs(enemies) do if v2 == v.Name then insert = false end end 
+        if insert then table.insert(enemies, v.Name) end 
     end
-    mobdropdown:Refresh(mobs)
+    dropdown:Refresh(enemies)
 end)
