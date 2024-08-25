92e522a921c93ee9dec447d2639d6b414734f43d
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:54:55 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,26 +4,11 @@ local Window = Library.CreateLib("List Mob", "BloodTheme")
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Select Auto Mob Farm")
-MONS = {} 
-
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
-table.insert(MONS,v.Name) 
-end
 
 Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(2.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = 
-game:GetService("Workspace").Enemies[].HumanoidRootPart.CFrame
+game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 end
     end)
-Section:NewDropdown("Please Select Monster", "", MONS, function(currentOption)
-Select = currentOption 
-end) 
-
-Section:NewButton("Refresh", "", function() 
-table.clear(MONS) 
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
-table.insert(MONS,v.Name) 
-end 
-end)
