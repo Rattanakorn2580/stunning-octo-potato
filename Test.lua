058e6c8ba28bea70540d670e8c02abf719301b7f
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 12:31:43 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -9,6 +9,6 @@ Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = 
-game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * CFrame.new(0,30,0)
+game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * CFrame.new(78.3136978, 225.999542, -249.492081, 0.584371388, 0, 0.811486363, 0, 1, 0, -0.811486363, 0, 0.584371388)
 end
     end)
