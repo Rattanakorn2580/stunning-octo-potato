aea70c963247a7abe38d43b99f5114135104822b
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:45:58 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,30 +1,13 @@
-
-
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("List Mob", "BloodTheme")
 
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Select Auto Mob Farm")
-MONS = {}
- 
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-    table.insert(MONS,v.Name)
-end
-Section:NewToggle("Auto Farm", "", function(state)
+
+Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
-    while _G.AutoFarm do wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv1 Crab"].HumanoidRootPart.CFrame
+    while _G.AutoFarm do wait(2.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 end
     end)
-
-Section:NewDropdown("Select Enemies", "", MONS, function(currentOption)
-    Select = currentOption
-end)
- 
-Section:NewButton("Refresh", "", function()
-    table.clear(MONS)
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-    table.insert(MONS,v.Name)
-end
-end)
