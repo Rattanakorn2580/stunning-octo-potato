2ed3252200575f319a2b5607580ccdcfee662cb8
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 18:55:16 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,9 +1,10 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Opl X", "BloodTheme")
 
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Auto Find")
 Section:NewToggle("Auto Find", "", function(state)
 _G.AutoFind = state 
-
 while _G.AutoFind do wait() 
 for i,v in pairs(game:GetService("Workspace").Compass.Handle:GetDescendants()) do 
 if v.Name == "TouchInterest" then 
