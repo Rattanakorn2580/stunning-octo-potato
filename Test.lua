636a0bd2c2cc170e45ec26907116f293f04253c9
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:11:13 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,3 +1,8 @@
+-- BYPASS TP
+if game:GetService("Players").LocalPlayer.Character.Services:FindFirstChild("Client") then
+    game:GetService("Players").LocalPlayer.Character.Services["Client"].Disabled = true
+end
+
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("List Mob", "BloodTheme")
 
