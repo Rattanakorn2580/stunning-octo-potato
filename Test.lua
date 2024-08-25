56fe9a962f49f2d1779147b3f29ed82859f24f5b
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:49:30 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,14 +1,14 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("List Mob", "BloodTheme")
 
+-- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Farm")
-Section:NewToggle("Lv2 Angry Bob", "", function(v)
-    _G.AutoFarm = v
-    while wait(1) do
-        if v.Humanoid.Health >= 0 then
-    repeat task.wait(1)
+local Section = Tab:NewSection("Select Auto Mob Farm")
+
+Section:NewToggle("Lv2 Angry Bob", "", function(state)
+    _G.AutoFarm = state
+    repeat task.wait(2.5) do
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 until v.Humanoid.Health <= 0 then
-end
-end)
+        end
+    end)
