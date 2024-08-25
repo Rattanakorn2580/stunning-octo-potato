eb616bc10049a6c0bc4a7ae700cadf3aa2d30dd7
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:28:34 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,9 +4,18 @@ local Window = Library.CreateLib("List Mob", "BloodTheme")
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
 
-Section:NewToggle("Lv20 Gunslinger", "", function(state)
+Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
     while _G.AutoFarm do wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv20 Gunslinger"].HumanoidRootPart.CFrame
-end
+            for i,v in pairs(game:GetService("Workspace").Enemies:GetDescendants()) do
+        if "Lv2 Angry Bob" then
+        if v.Humanoid.Health >= 0 then
+    repeat task.wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+until _G.AutoFarm false or v.Humanoid.Health <= 0 then
+                    end
+                end
+            end
+        end
     end)
+                        
