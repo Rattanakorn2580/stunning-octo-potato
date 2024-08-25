72c28418b8317051e127a009cb7623726e211bae
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 13:22:46 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -2,14 +2,20 @@
 
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("List Mob", "BloodTheme")
+
+-- AUTO FARM
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Select Auto Mob Farm")
 -- BYPASS TP
 if game:GetService("Players").LocalPlayer.Character.Services:FindFirstChild("Client") then
     game:GetService("Players").LocalPlayer.Character.Services["Client"].Disabled = true
 end
 
--- AUTO FARM
-local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Select Auto Mob Farm")
+MONS = {}
+ 
+for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+    table.insert(MONS,v.Name)
+end
 
 Section:NewToggle("Lv2 Angry Bob", "", function(state)
     _G.AutoFarm = state
