aa97c7af1292a7790cb3a8fb31b50a8e51495716
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:40:49 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -5,16 +5,11 @@ local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Auto Farm")
 Section:NewToggle("Lv2 Angry Bob", "", function(v)
     _G.AutoFarm = v
-    while _G.AutoFarm do wait(1)
-            for i,v in pairs(game:GetService("Workspace").Enemies:GetDescendants()) do
-        if v.Name = "Lv2 Angry Bob" then
+    while wait(1) do
         if v.Humanoid.Health >= 0 then
     repeat task.wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
 until v.Humanoid.Health <= 0 then
 end
-end
-end
-end
 end)
-                        
+end                
