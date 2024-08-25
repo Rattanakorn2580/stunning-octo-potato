0b3b73829acae6cda381f0d78c2620d7fa0c8499
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-25 16:18:01 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,34 +1,11 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("List Mob", "BloodTheme")
 
--- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Select Auto Mob Farm")
-
-Section:NewToggle("Auto Farm", "", function(state)
-  _G.AutoFarm = state
- while _G.AutoFarm do wait(1)
-  for i,v in pairs(game:GetService("Workspace").Enemies:Descendants()) do
-  if v.Name == "Lv2 Angry Bob" then
-  if Humanoid.Health >= 0 then
-  repeat task.wait()
-  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
-        until _G.AutoFarm false or v.Humanoid.Health <= 0 then
-       end
-      end
-     end)
-   end
-
-Section:NewToggle("Lv1 Crab", "", function(state)
-  _G.AutoFarm = state
- while _G.AutoFarm do wait(1)
-  for i,v in pairs(game:GetService("Workspace").Enemies:Descendants()) do
-  if v.Name == "Lv1 Crab" then
-  if Humanoid.Health >= 0 then
-  repeat task.wait()
-  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
-        until _G.AutoFarm false or v.Humanoid.Health <= 0 then
-       end
-      end
-     end)
-   end
+local Section = Tab:NewSection("Auto Farm")
+Section:NewToggle("Lv20 Gunslinger", "", function(state)
+    _G.AutoFarm = state
+    while _G.AutoFarm do wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv20 Gunslinger"].HumanoidRootPart.CFrame
+end
+    end)
