87e9a78e24338878336ecd0a8faef9136a2c4e63
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-26 19:42:17 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,48 +1,30 @@
--- Gui to Lua
--- Version: 3.2
+-- ScreenGui
+local ScreenGui = Instance.new("ScreenGui") 
+ScreenGui.Name = "ScreenGui" 
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") 
+ScreenGui.ResetOnSpawn = false 
 
--- Instances:
+local Toggle = Instance.new("TextButton") 
+Toggle.Name = "Toggle" 
+Toggle.Parent = ScreenGui 
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0) 
+Toggle.Size = UDim2.new(0, 90, 0, 38) 
+Toggle.Font = Enum.Font.SourceSans 
+Toggle.Text = "Tutorial" 
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248) 
+Toggle.TextSize = 28.000 
+Toggle.Draggable = true 
+Toggle.MouseButton1Click:connect(function() 
+Library:ToggleUI() 
+end) 
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextButton = Instance.new("TextButton")
-local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(26, 26, 26)
-Frame.BackgroundTransparency = 0.500
-Frame.Position = UDim2.new(0.858712733, 0, 0.0237762257, 0)
-Frame.Size = UDim2.new(0.129513338, 0, 0.227972031, 0)
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.BackgroundTransparency = 1.000
-TextButton.Size = UDim2.new(1, 0, 1, 0)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "Example"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextScaled = true
-TextButton.TextSize = 50.000
-TextButton.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextStrokeTransparency = 0.000
-TextButton.TextWrapped = true
-TextButton.MouseButton1Down:Connect(function()
-	game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" , false , game)
-end)
-
-UITextSizeConstraint.Parent = TextButton
-UITextSizeConstraint.MaxTextSize = 50
+local Corner = Instance.new("UICorner") 
+Corner.Name = "Corner" 
+Corner.Parent = Toggle
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Opl X", "DarkTheme")
 
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("Select Player!")
-Section1:NewButton("Toggle ui", "Toggle the ui yes", Enum.KeyCode.F6, function()
-    Library:ToggleUI()
-end)
