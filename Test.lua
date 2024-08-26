563f07559e34695307c8807588938f4841c2f322
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-26 15:17:57 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,15 +1,13 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Opl X", "BloodTheme")
+local Window = Library.CreateLib("List Mob", "BloodTheme")
 
+-- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Auto Find")
-Section:NewToggle("Auto Find", "", function(state)
-_G.AutoFind = state 
-while _G.AutoFind do wait() 
-for i,v in pairs(game:GetService("Workspace").Compass.Handle:GetDescendants()) do 
-if v.Name == "TouchInterest" then 
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame 
-end 
-end 
-end 
-end)
+local Section = Tab:NewSection("Select Auto Mob Farm")
+
+Section:NewToggle("Lv2 Angry Bob", "", function(state)
+    _G.AutoFarm = state
+    while _G.AutoFarm do wait(2.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame
+end
+    end)
