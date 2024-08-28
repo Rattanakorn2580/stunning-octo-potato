e88b914ebd98ab8ceaf9d1c550a4db0b2b78b1fc
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-08-28 09:04:00 +0700

    Update Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -22,22 +22,22 @@ Library:ToggleUI()
 end)
 
 -- KILL AURA
-local Tab = Window:NewTab("Mob Farm")
-local Section = Tab:NewSection("Kill Aura")
-
-Section:NewToggle("Auto Click", " ", function(q) 
-     getgenv().KillAura = q
-    end)
 spawn(function()
 while wait() do
     if getgenv().KillAura == true
 pcall(function()
 local Vector3 = Vector3.new(-9653.310546875, 47.48046112060547, -4440.38330078125)
 local CFrame = CFrame.new(-9653.31055, 47.4804611, -4440.3833, 0.345474839, -0.00910704955, 0.938383877, -0, 0.999953032, 0.00970457867, -0.938428104, -0.00335268769, 0.345458567)
-
 game:GetService("ReplicatedStorage").RemoteEvents.BladeCombatRemote:FireServer(false, ohVector32, ohCFrame3)
   end
-  end
-end)
+  end)
 end
-        end)
+end)
+
+local Tab = Window:NewTab("Mob Farm")
+local Section = Tab:NewSection("Kill Aura")
+
+Section:NewToggle("kill aura", " ", function(true) 
+     getgenv().KillAura == true
+    end)
+
