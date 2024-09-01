7b1bfe40591ef65f943b48571df414cf3e072a4f
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-02 00:30:55 +0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -31,30 +31,8 @@ end)
 
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Select Mob Farm")
-
-Section:NewToggle("Lv2 Angry Bob", "", function(state)
-    _G.AutoFarm = state
-while wait() do -- LOOP WHICH REPEATS UNTIL THE UNTIL IS TRUE / DONE
-                wait() -- WAIT SO WE DONT CRASH
-                if _G.AutoFarm == false then return end -- IF THE TOGGLE IS OFF THEN STOP THE LOOP
-                if game:GetService("Workspace").Enemies:FindFirstChild() ~= nil then break; end
-            end -- IF THE MOB IS SPAWNED THEN GO ON WITH THE AUTOFARM
-        else
-            while wait() do
-                _G.AutoFarm = game:GetService("Workspace").Enemies["Lv2 Angry Bob"]:FindFirstChild()
-                if _G.AutoFarm ~= then break; end
-                if G.AutoFarm == false then return end -- IF THE TOGGLE IS OFF THEN STOP THE LOOP
-                if _G.AutoFarm ~= nil then
-                    if game:FindFirstChild("Humanoid") then
-                        if game:GetService("Workspace").Enemies["Lv2 Angry Bob"].Humanoid.Health == 0 then wait() game:Destroy() break; end -- IF THE MOB IS DEAD THEN JUST DESTROY IT FOR FASTER FARMING
-                    end
-                    if game:FindFirstChild("HumanoidRootPart") then
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * CFrame.new(0,0,2) -- TELEPORT TO THE MOB
-                    end
-                end
-                wait() -- WAIT SO WE DONT CRASH
-            end
-        end
-    end
-end)
+local Section = Tab:NewSection("Lv2 Angry Bob")
+while _G.AutoFarm do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * New.CFrame(0,0,5)
+end
+    end)
