c66fe78fe3763068eb5e8a553691fbf7448c7bf0
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-05 17:08:59 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,18 +1,4 @@
-spawn(function()
-    while wait() do 
-        pcall(function()
-            if _G.Raids then
-                for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-                    if v.ClassName == "Model" and v.Humanoid.Health > 0 then
-			v.Humanoid.Health = 0
-                    end
-                end
-            end
-        end)
-    end
-end)
- 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- อันนี้เป็นUI
+ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- อันนี้เป็นUI
 local Window = Library.CreateLib("Power x Hub", "Synapse") -- อันนี้เป็นชื่อui
 
 local ScreenGui = Instance.new("ScreenGui") 
@@ -34,67 +20,16 @@ Toggle.Draggable = true
 Toggle.MouseButton1Click:connect(function() 
 Library:ToggleUI() 
 end)
-spawn(function()
-        while wait() do
-            pcall(function()
-                    if _G.HitMob then
-                        for i,v in 
-pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
-      v.HumanoidRootPart.Size = Vector3.new(13) 
-      v.HumanoidRootPart.Transparency = 0.5
-                        end
-                    end
-                end)
-            end
-        end)
 
 local Drinklist = {} 
 local Drink = nil
-
 for i,v in 
 pairs(game:GetService("Workspace").Merchant.DrinkBetterMerchant:GetChildren()) do 
     table.insert(Drinklist,v.Name) 
 end 
 
-local Tab = Window:NewTab("Main")-- อันนี้เป็นชื่อของTab
-local Section = Tab:NewSection("Rain") -- อันนีเป็นชื่อของSection 
-Section:NewToggle("Kill all", "", function(v) -- อันนี้เป็น NewToggle
-    _G.Raids = v --อันนี้เป็นตัวแปร
-end)
-Section:NewToggle("Hitbox Mob", "", function(v)
-        _G.HitMob = v
-    end)
-
-Section:NewButton("Noclip","", function()
-  local Noclip = nil
-local Clip = nil
-
-function noclip()
-	Clip = false
-	local function Nocl()
-		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
-			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
-					v.CanCollide = false
-				end
-			end
-		end
-		wait(0.21) -- basic optimization
-	end
-	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
-end
-
-function clip()
-	if Noclip then Noclip:Disconnect() end
-	Clip = true
-end
-noclip() -- to toggle noclip() and clip()
-end)
-
-Section:NewButton("Admin","", function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
-end)
-
+local Tab = Window:NewTab("NPC")
+local Section = Tab:NewSection("NCP DrinkBuyer")
 Section:NewDropdown("List Drink", " ", Drinklist, function(currentOption) 
         Drink = currentOption 
     end)
