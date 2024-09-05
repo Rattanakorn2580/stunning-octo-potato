fa7381b6a5926da3bc39baae0a3ea4cd34bf0f8f
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-05 09:08:34 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -23,7 +23,7 @@ end)
 
 -- UPDATE
 local Tab = Window:NewTab("Update")
-local Section = Tab:NewSection("เพิ่มมอนHITBOXเข้ามา | ยังไม่สมบูรณ์")
+local Section Tab:NewTabSection("เอาhitboxออก เพิ่ม -Bring mob เข้ามา | ยังไม่สมบูรณ์")
 
 -- AUTO FARM
 local Tab = Window:NewTab("Auto Farm")
@@ -69,28 +69,11 @@ end
 end 
 end)
 
-spawn(function()
-        while wait() do
-            pcall(function()
-                    if _G.HitMob then
-                        for i,v in 
-pairs(game:GetService("Workspace").Enemies:GetChildren()) do 
-      v.HumanoidRootPart.Size = Vector3.new(35, 35, 35) 
-      v.HumanoidRootPart.Transparency = 0.5 
-                        end
-                    end
-                end)
-            end
-        end)
-
 local Section = Tab:NewSection("Auto Farm")
-Section:NewToggle("Farm All","", function(t)
-	_G.AutoFarm = t
+Section:NewToggle("BringMob List","", function(t)
+	_G.Bring = t
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/main/Hub.lua"))()
 end)
-Section:NewToggle("HitBox Mob", " ", function(t)
-	_G.HitMob = t
-	end)
 
 local Section = Tab:NewSection("Auto Sam")
 Section:NewToggle("Auto Claim10", " ", function(a) 
