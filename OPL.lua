9da02d95c3a0c1130b4dde726e993248f3c33fc1
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-05 17:10:09 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -274,6 +274,33 @@ Section:NewButton("Rejoin","", function()
   game.Players.LocalPlayer:Kick()
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end)
+
+Section:NewButton("Noclip","", function()
+  local Noclip = nil
+local Clip = nil
+
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
+end
+noclip() -- to toggle noclip() and clip()
+end)
+
 Section:NewButton("BoostFPS","BoostFPS", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/8YZ2cc6V"))()
 end)
