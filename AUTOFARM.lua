419b259460922ce92476f589217ebc935b799bd9
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-06 18:11:26 +0700

    Update AUTOFARM.lua

--- AUTOFARM.lua
+++ AUTOFARM.lua
@@ -25,30 +25,30 @@ end)
 local Tab = Window:NewTab("Taget List")
 local Section = Tab:NewSection("Select Mob")
 Section:NewToggle("Lv2 Angry Bob", " ", function(state) 
-        _G.AutoFarmLV = state ----true/false
+        _G.AutoFarmLV = state
         end)
-MON = "Lv2 Angry Bob" --MonName
+MON = "Lv2 Angry Bob"
  
     spawn(function()
        game:GetService("RunService").RenderStepped:Connect(function()
         pcall(function()
             if _G.AutoFarmLV then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[MON].HumanoidRootPart.CFrame * CFrame.new(0,0,3) --Distace
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[MON].HumanoidRootPart.CFrame * CFrame.new(0,0,3)
             end
         end)
        end)
     end)
 
-Section:NewToggle("Lv22 Angry Bobby", " ", function(s) 
-        _G.AutoFarmL = s ----true/false
+Section:NewToggle("Lv22 Angry Bobby", " ", function(stats) 
+        _G.AutoFarmV = stats
         end)
-MOND = "Lv22 Angry Bobby" --MonName
+MOND = "Lv22 Angry Bobby"
  
     spawn(function()
        game:GetService("RunService").RenderStepped:Connect(function()
         pcall(function()
-            if _G.AutoFarmL then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[MOND].HumanoidRootPart.CFrame * CFrame.new(0,0,3) --Distace
+            if _G.AutoFarmV then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[MOND].HumanoidRootPart.CFrame * CFrame.new(0,0,3)
             end
         end)
        end)
