9dbe10227c20b680a05bfed489da27a366694712
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-06 11:06:54 +0700

    Update AUTOFARM.lua

--- AUTOFARM.lua
+++ AUTOFARM.lua
@@ -24,28 +24,19 @@ end)
 --AUTOFARM
 local Tab = Window:NewTab("ListMob")
 local Section = Tab:NewSection("List MobFarm")
-spawn(function() 
-while task.wait(.1) do 
-pcall(function() 
-if AutoFarm then
-for i,v in pairs(game:GetService("Workspace").Enemies["Lv2 Angry Bob"]:GetChildren()) do
-if v.Humanoid.Health == 0 then
-end 
-end 
-end
-end) 
-end 
-end)
+
 Section:NewToggle("Lv2 Angry Bob", " ", function(t) 
     AutoFarm = t
     end)
 
-spawn(function()
-    while wait(.1) do
-        pcall(function()
-            if AutoFarm then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * CFrame.new(0,0,3)
-            end
-        end)
-    end
+spawn(function() 
+while task.wait(.1) do 
+pcall(function() 
+if AutoFarm then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies["Lv2 Angry Bob"].HumanoidRootPart.CFrame * CFrame.new(0,0,3)
+if v.Humanoid.Health == 0 then
+end
+end
+end) 
+end 
 end)
