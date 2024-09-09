8aec13a534ad34ccb39eb56dcddea537105b3378
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 10:59:12 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,41 +1,9 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("OPLMIUM HUB", "DarkTheme")
+local NovoBloco = Instance.new("Part", workspace)
+NovoBloco.Anchored = true
+NovoBloco.CanCollide = true
+NovoBloco.Size = Vector3.new(200, 200, 200)
+_G.e = true
 
-local ScreenGui = Instance.new("ScreenGui") 
-ScreenGui.Name = "ScreenGui" 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") 
-ScreenGui.ResetOnSpawn = false 
-
-local Toggle = Instance.new("TextButton") 
-Toggle.Name = "Toggle" 
-Toggle.Parent = ScreenGui 
-Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
-Toggle.Position = UDim2.new(0, 0, 0.454706937, 0) 
-Toggle.Size = UDim2.new(0, 90, 0, 38) 
-Toggle.Font = Enum.Font.SourceSans 
-Toggle.Text = "Turn/Off" 
-Toggle.TextColor3 = Color3.fromRGB(248, 248, 248) 
-Toggle.TextSize = 28.000 
-Toggle.Draggable = true 
-Toggle.MouseButton1Click:connect(function() 
-Library:ToggleUI() 
-end)
-
-Section:NewToggle("Auto Bring Compass", " ", function()
-local plr=game:GetService'Players'.LocalPlayer 
-
-	local dftofind='Compass'
-	while true do 
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, Players.LocalPlayer.Backpack:GetChildren() do 
-	local Poser=v:FindFirstChild'Poser' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Poser then 
-	HRP.CFrame=Poser.CFrame 
-	end 
-	end 
-	wait(1) 
-	end 
-
-
-end)
+while _G.e == true do wait()
+	{CFrame = game.Players.LocalPlayer.Character:MoveTo(Vector3.new(88.5, 297.100006, -916, 0, 0, -1, 0, 1, 0, 1, 0, 0))}):Play()
+end
