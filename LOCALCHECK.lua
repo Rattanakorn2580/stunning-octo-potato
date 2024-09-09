939b171104613f2fe4bbc349b6b62ddf6ef73040
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 15:05:54 +0700

    Update LOCALCHECK.lua

--- LOCALCHECK.lua
+++ LOCALCHECK.lua
@@ -2,8 +2,5 @@ local Pl = game.PlaceId
 if Pl == 8569358381 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/main/BetaOPMIUM.lua"))();
 else
-        game.Players.LocalPlayer:Kick("Script Not Support")
-        wait(1)
-	    game:GetService("TeleportService"):Teleport(game.Pl, 
-		game:GetService("Players").LocalPlayer)
+        game.Players.LocalPlayer:Kick()
 end
