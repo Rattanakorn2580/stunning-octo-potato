2b727c61f94b4c28384bf11711e70ab86dfb84be
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 12:19:07 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -353,20 +353,27 @@ end
 noclip() -- to toggle noclip() and clip()
 end)
 
-Section:NewToggle("Auto tp Safezone", " ", function(se
-        _G.e = se
+Section:NewToggle("Auto tp Safezone", " ", function(se)
+local NovoBloco = Instance.new("Part", workspace) 
+local ts = game:GetService("TweenService") 
+NovoBloco.Anchored = true 
+NovoBloco.CanCollide = true 
+NovoBloco.Size = Vector3.new(250.246, 0.771, 250.852) 
+_G.e = se
+	end)
+
+    spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.e then
+ts:Create(NovoBloco, TweenInfo.new(0.2), 
+		{CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, -3.5, 0)}):Play() 
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(4500.5, 10000, 4500, 0, 0, -1, 0, 1, 0, 1, 0, 0))
+            end
+        end)
+       end)
     end)
-
-spawn(function()
-while wait() do 
-if AutoClicked then 
-pcall(function() 
-game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672)) 
-end) 
-end 
-end 
-end)
-
+			
 Section:NewButton("BoostFPS","BoostFPS", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/8YZ2cc6V"))()
 end)
