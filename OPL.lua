33125bf8a5a0fc41e2b5d9b6dfd2810cf428c7d1
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 12:42:13 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -357,7 +357,7 @@ local NovoBloco = Instance.new("Part", workspace)
 local ts = game:GetService("TweenService") 
 NovoBloco.Anchored = true 
 NovoBloco.CanCollide = true 
-NovoBloco.Size = Vector3.new(250.246, 0.771, 250.852) 
+NovoBloco.Size = Vector3.new(200.246, 0.771, 200.852) 
 _G.e = se
 
     spawn(function()
@@ -366,13 +366,13 @@ _G.e = se
             if _G.e then
 ts:Create(NovoBloco, TweenInfo.new(0.2), 
 		{CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, -3.5, 0)}):Play() 
-game.Players.LocalPlayer.Character:MoveTo(Vector3.new(4500.5, 10000, 4500, 0, 0, -1, 0, 1, 0, 1, 0, 0))
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(5000.5, 45000, 5000, 0, 0, -1, 0, 1, 0, 1, 0, 0))
             end
         end)
        end)
     end)
 
-Section:NewToggle("Auto tp Safezone", " ", function(se)
+Section:NewToggle("Tp Safezone", " ", function(se)
 	_G.e = se
 	end)
 			
