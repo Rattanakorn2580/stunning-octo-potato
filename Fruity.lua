ac0b5e550f0c6e789ac74d7bcabcb62f467b0e67
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 21:41:37 +0700

    Update Fruity.lua

--- Fruity.lua
+++ Fruity.lua
@@ -3,106 +3,17 @@ _G.Fruity = ft
 local plr=game:GetService'Players'.LocalPlayer 
 
 	local dftofind='Apple'
-	while _G.Fruity do wait()
-	if _G.Fruity then
+	while _G.Fruity do 
 	local char=plr.Character or plr.CharacterAdded:Wait() 
 	local HRP=char:WaitForChild'HumanoidRootPart' 
 	for i,v in next, workspace:GetChildren() do 
 	local Handle=v:FindFirstChild'Handle' 
 	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFram
-	end
+	Handle.CFrame=HRP.CFrame 
 	end 
 	end 
 	wait(1) 
 	end 
 
-	local dftofind='Green Apple'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
-	end
-	end 
-	end 
-	wait(1) 
-	end 
 
-	local dftofind='Coconut'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
-	end
-	end 
-	end 
-	wait(1) 
-	end  
-
-	local dftofind='Banana'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
 end
-	end 
-	end 
-	wait(1) 
-	end  
-
-	local dftofind='Cantaloupe'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
-end
-	end 
-	end 
-	wait(1) 
-	end  
-
-	local dftofind='Pumpkin'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
-end
-	end 
-	end 
-	wait(1) 
-	end 
-
-	local dftofind='Melon'
-	while _G.Fruity do wait()
-if _G.Fruity then
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame
-end
-	end 
-	end 
-	wait(1) 
-	end
