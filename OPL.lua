5aa80c62300f96b4934a64b8bae175430920e20a
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-09 12:28:07 +0700

    Update OPL.lua

--- OPL.lua
+++ OPL.lua
@@ -353,16 +353,13 @@ end
 noclip() -- to toggle noclip() and clip()
 end)
 
-Section:NewToggle("Auto tp Safezone", " ", function(se) 
-_G.e = se
-	end)
-
+Section:NewButton("Auto tp Safezone", " ", function()
 local NovoBloco = Instance.new("Part", workspace) 
 local ts = game:GetService("TweenService") 
 NovoBloco.Anchored = true 
-NovoBloco.CanCollide = true 
+NovoBloco.CanCollide = true
 NovoBloco.Size = Vector3.new(250.246, 0.771, 250.852)
-
+_G.e = true
     spawn(function()
        game:GetService("RunService").RenderStepped:Connect(function()
         pcall(function()
@@ -370,7 +367,7 @@ NovoBloco.Size = Vector3.new(250.246, 0.771, 250.852)
 ts:Create(NovoBloco, TweenInfo.new(0.2), 
 		{CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, -3.5, 0)}):Play() 
 game.Players.LocalPlayer.Character:MoveTo(Vector3.new(4500.5, 10000, 4500, 0, 0, -1, 0, 1, 0, 1, 0, 0))
-            end
+            end)
         end)
        end)
     end)
