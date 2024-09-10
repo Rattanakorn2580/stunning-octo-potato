fdaeefcdecd8323f781f626125a4b835304c9ec2
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-10 14:06:31 +0700

    Update and rename LOCALCHECK.lua to OPHub.lua

--- LOCALCHECK.lua
+++
@@ -1,6 +0,0 @@
-local Pl = game.PlaceId
-if Pl == 8569358381 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/main/BetaOPMIUM.lua"))();
-else
-        game.Players.LocalPlayer:Kick()
-end
