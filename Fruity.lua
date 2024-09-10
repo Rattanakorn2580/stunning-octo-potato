b9458ddcc1e11d68ffb26911fb32fbe69e8c7d01
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-10 13:05:50 +0700

    Update Fruity.lua

--- Fruity.lua
+++ Fruity.lua
@@ -1,46 +1,32 @@
 _G.Fruity = ft
-
-local plr=game:GetService'Players'.LocalPlayer 
-
-	local dftofind='Apple'
-	while true do 
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame 
-	end 
-	end 
-	wait(1) 
-	end 
-
-local plr=game:GetService'Players'.LocalPlayer 
-
-	local dftofind='Green Apple'
-	while true do 
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame 
-	end 
-	end 
-	wait(1) 
-	end 
-
-local plr=game:GetService'Players'.LocalPlayer 
-
-	local dftofind='Golden Apple'
-	while true do 
-	local char=plr.Character or plr.CharacterAdded:Wait() 
-	local HRP=char:WaitForChild'HumanoidRootPart' 
-	for i,v in next, workspace:GetChildren() do 
-	local Handle=v:FindFirstChild'Handle' 
-	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
-	Handle.CFrame=HRP.CFrame 
-	end 
-	end 
-	wait(1) 
-	end 
+while _G.Fruity do wait()
+if not fruit:WaitForChild("Handle", 1) then return end
+	local gui = Instance.new("BillboardGui", fruit.Handle)
+	gui.AlwaysOnTop = true
+	gui.Size = UDim2.new(0, 60, 0, 20)
+	gui.Enabled = true
+	local text = Instance.new("TextLabel", gui)
+	text.Size = UDim2.new(1, 0, 1, 0)
+	text.BackgroundColor3 = Color3.new(255, 255, 255)
+	text.TextScaled = true
+	text.Text = fruit.Name
+end
+ 
+function isFruit(tool)
+	if string.find(tool.Name, "Fruit") then
+		return true
+	end
+	return false
+end
+ 
+for k, v in pairs(workspace:GetChildren()) do
+	if v.ClassName == "Tool" and isFruit(v) then
+		hook(v)
+	end
+end
+ 
+workspace.ChildAdded:Connect(function(child)
+	if child.ClassName == "Tool" and isFruit(child) then
+		hook(child)
+	end
+end)
