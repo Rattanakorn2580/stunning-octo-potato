d9d178020eeff87fb1d6b12aab06c36112797c35
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-10 13:11:37 +0700

    Update Fruity.lua

--- Fruity.lua
+++ Fruity.lua
@@ -1,1 +1,31 @@
+_G.Fruity = ft
 
+local plr=game:GetService'Players'.LocalPlayer 
+
+	local dftofind='Apple'
+	while true do 
+	local char=plr.Character or plr.CharacterAdded:Wait() 
+	local HRP=char:WaitForChild'HumanoidRootPart' 
+	for i,v in next, workspace:GetChildren() do 
+	local Handle=v:FindFirstChild'Handle' 
+	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
+	Handle.CFrame=HRP.CFrame 
+	end 
+	end 
+	wait(1) 
+	end 
+
+local plr=game:GetService'Players'.LocalPlayer 
+
+	local dftofind='Green Apple'
+	while true do 
+	local char=plr.Character or plr.CharacterAdded:Wait() 
+	local HRP=char:WaitForChild'HumanoidRootPart' 
+	for i,v in next, workspace:GetChildren() do 
+	local Handle=v:FindFirstChild'Handle' 
+	if v:IsA'Tool' and string.find(v.Name:lower(), dftofind:lower()) and Handle then 
+	Handle.CFrame=HRP.CFrame 
+	end 
+	end 
+	wait(1) 
+	end 
