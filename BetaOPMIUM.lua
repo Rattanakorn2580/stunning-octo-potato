3dd73c2c4d12063b8ecea73e36772e505c4f6d12
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-10 19:39:31 +0700

    Update BetaOPMIUM.lua

--- BetaOPMIUM.lua
+++ BetaOPMIUM.lua
@@ -307,7 +307,162 @@ Section:NewToggle("Auto Bring Fruits", "", function(ft)
 	_G.Fruity = ft
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/main/Fruity.lua"))()
 		end)
-	
+
+Section:NewToggle("Auto Mixer", " ", function(mix)
+        _G.autofruit = mix
+    end)
+
+spawn(function()--autofruit
+    while wait() do
+        pcall(function()
+            if _G.autofruit then
+                wait(1.5)
+                for i,v in pairs(game:GetService("Workspace").Barrels.Crates:GetDescendants()) do
+                    if v:IsA("ClickDetector") then
+                        fireclickdetector(v)
+                    end
+                end
+                for i,v in pairs(game:GetService("Workspace").Barrels.Barrels:GetDescendants()) do
+                    if v:IsA("ClickDetector") then
+                        fireclickdetector(v)
+                    end
+                end
+            end
+        end)
+    end
+end)
+spawn(function()--autofruit
+    while wait() do
+    pcall(function()
+        if _G.autofruit then
+            local Players = game:GetService("Players")
+            local LocalPlayer = Players.LocalPlayer
+            local PopperClient = LocalPlayer:WaitForChild("PlayerScripts").PlayerModule.CameraModule.ZoomController.Popper
+            
+            for i, v in next, getgc() do
+                if getfenv(v).script == PopperClient and typeof(v) == "function" then
+                    for i2, v2 in next, debug.getconstants(v) do
+                        if tonumber(v2) == 0.25 then
+                            debug.setconstant(v, i2, 0)
+                       -- elseif tonumber(v2) == 0 then
+                           -- debug.setconstant(v, i2, 0.25)
+                        end
+                    end
+                end
+            end
+            --Barrels
+            wait(1)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-12, 216, -351)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4, 216, -378)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-114, 216, -750)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-133, 216, -710)
+            wait(1.5)
+    
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-204, 224, -206)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1754, 217, -327)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1706, 217, -326)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1754, 216, -217)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1550, 217, -307)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1494, 217, -306)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1493, 217, -290)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1871, 218, 823)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1858, 218, 814)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1882, 219, 838)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1989, 235, 568)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1970, 219, 576)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1989, 218, 561)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1981, 217, 553)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1517, 217, -289)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1186, 217, -285)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1173, 217, -286)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1009, 220, 3342)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2601, 254, 1111)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2618, 254, 1110)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(989, 224, -3337)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(916, 216, 3409)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(882, 218, 3364)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1167, 219, 3228)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(987, 224, -3337)
+            wait(1.5)
+            --[[
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(987, 224, -3337)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1072, 224, -3429)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1195, 224, -3372)
+            wait(1.5)
+
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1238, 224, -3231)
+            wait(1.5)]]
+            for i,v in pairs(game.Workspace:GetChildren()) do 
+                if v.ClassName == "Tool" then
+                    v.Handle.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                end
+            end
+            wait(1.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1193, 216, 630)
+            wait(5)
+            end
+        end)
+    end
+end)
+
 -- TP ISLAND
 local Tab = Window:NewTab("TP Island")                       
 local Section = Tab:NewSection("TP Island")
