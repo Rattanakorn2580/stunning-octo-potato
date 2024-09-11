c163cba7f41ac9b3112de9f2f5f7d53d55ae75c5
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-11 14:26:54 +0700

    Update OPHub.lua

--- OPHub.lua
+++ OPHub.lua
@@ -192,6 +192,26 @@ function create:Win(text)
                     library.toggledui = false
                     tween:Create(MainSceen,TweenInfo.new(0.4,Enum.EasingStyle.Back),{Size = UDim2.new(0, 534, 0, 474)}):Play()
                     Reduisceen.Enabled = true
+
+local ScreenGui = Instance.new("ScreenGui") 
+ScreenGui.Name = "ScreenGui" 
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") 
+ScreenGui.ResetOnSpawn = false 
+
+local Toggle = Instance.new("TextButton") 
+Toggle.Name = "Toggle" 
+Toggle.Parent = ScreenGui 
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0) 
+Toggle.Size = UDim2.new(0, 90, 0, 38) 
+Toggle.Font = Enum.Font.SourceSans 
+Toggle.Text = "Turn/Off" 
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248) 
+Toggle.TextSize = 28.000 
+Toggle.Draggable = true 
+Toggle.MouseButton1Click:connect(function() 
+Library:ToggleUI() 
+                   end)
                 end
             end
         end)
