af37db58e51c771fe5bf4673aff098a446938d63
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-21 00:21:09 +0700

    Update OrionByBankZy.lua

--- OrionByBankZy.lua
+++ OrionByBankZy.lua
@@ -471,14 +471,14 @@ function OrionLib:MakeWindow(WindowConfig)
 	local UIHidden = false
 
 	WindowConfig = WindowConfig or {}
-	WindowConfig.Name = WindowConfig.Name or "OPlium Hub"
+	WindowConfig.Name = WindowConfig.Name or "OPlium Hub By Bank Kesee"
 	WindowConfig.ConfigFolder = WindowConfig.ConfigFolder or WindowConfig.Name
 	WindowConfig.SaveConfig = WindowConfig.SaveConfig or false
 	WindowConfig.HidePremium = WindowConfig.HidePremium or false
 	if WindowConfig.IntroEnabled == nil then
 		WindowConfig.IntroEnabled = true
 	end
-	WindowConfig.IntroText = WindowConfig.IntroText or "OPlium Hub"
+	WindowConfig.IntroText = WindowConfig.IntroText or "OPlium Hub By Bank Kesee"
 	WindowConfig.CloseCallback = WindowConfig.CloseCallback or function() end
 	WindowConfig.ShowIcon = WindowConfig.ShowIcon or false
 	WindowConfig.Icon = WindowConfig.Icon or "rbxassetid://8834748103"
