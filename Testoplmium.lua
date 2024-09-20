16771f1e9b62aaf0c01b1b9c6a07d21560c012c6
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-20 10:16:16 +0700

    Update Testoplmium.lua

--- Testoplmium.lua
+++ Testoplmium.lua
@@ -6,12 +6,12 @@ Cache.DevConfig["ListOfBox"] = {"Common Box", "Uncommon Box"};
 Cache.DevConfig["ListOfDrink"] = {"Cider+", "Lemonade+", "Juice+", "Smoothie+"}; 
 Cache.DevConfig["ListOfDrinkFormMixer"] = {"Cider", "Lemonade", "Juice", "Smoothie", "Milk", "Golden Apple"};
 
-local Tab = Window:MakeTab({ 	
+local Tab1 = Window:MakeTab({ 	
         Name = "Autos", 	
         Icon = "rbxassetid://4483345998", 	
         PremiumOnly = false })
 
-local Tab = Window:MakeTab({ 	Name = "Auto Farm", 	
+local Tab2 = Window:MakeTab({ 	Name = "Auto Farm", 	
     Icon = "rbxassetid://4483345998", 	
     PremiumOnly = false })
 
