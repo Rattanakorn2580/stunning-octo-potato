a7dffe109f1d5761015e2ac60c41624653f8be9d
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-09-23 23:16:14 +0700

    Update Mater.lua

--- AutoHakiFaster/Oplium/Mater.lua
+++ AutoHakiFaster/Oplium/Mater.lua
@@ -1,1 +1,30 @@
 AutoHaki2 = AH2
+
+spawn(function()
+    while wait() do
+        pcall(function()
+            if AutoHaki2 then
+                for i = 1, 2 do
+                    local args = {
+                        [1] = "On",
+                        [2] = 300
+                    }
+                    workspace:WaitForChild("UserData"):WaitForChild("User_" .. game.Players.LocalPlayer.UserId):WaitForChild("III"):FireServer(unpack(args))
+                    task.wait(0.1)
+                    local args = {
+                        [1] = "Off",
+                        [2] = 300
+                    }
+                    workspace:WaitForChild("UserData"):WaitForChild("User_" .. game.Players.LocalPlayer.UserId):WaitForChild("III"):FireServer(unpack(args))
+                    task.wait(0.1)
+                    local args = {
+                        [1] = "Off",
+                        [2] = 300
+                    }
+                    workspace:WaitForChild("UserData"):WaitForChild("User_" .. game.Players.LocalPlayer.UserId):WaitForChild("III"):FireServer(unpack(args))
+        end
+      end
+    end)
+end
+end);
+
