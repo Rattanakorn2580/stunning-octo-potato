cbb4351a3e50c7dcb34ff0ead3d0e2975960808e
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-01 22:18:24 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -1728,6 +1728,84 @@ TabSV:AddButton({
   	end    
 })
 
+TabSV:AddButton({
+	Name = "Rejoin Server",
+	Callback = function()
+        local PlaceID = game.PlaceId
+          local AllIDs = {}
+          local foundAnything = ""
+          local actualHour = os.date("!*t").hour
+          local Deleted = false
+          --[[
+          local File = pcall(function()
+              AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+          end)
+          if not File then
+              table.insert(AllIDs, actualHour)
+              writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+          end
+          ]]
+          function TPReturner()
+              local Site;
+              if foundAnything == "" then
+                  Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+              else
+                  Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+              end
+              local ID = ""
+              if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+                  foundAnything = Site.nextPageCursor
+              end
+              local num = 0;
+              for i,v in pairs(Site.data) do
+                  local Possible = true
+                  ID = tostring(v.id)
+                  if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                      for _,Existing in pairs(AllIDs) do
+                          if num ~= 0 then
+                              if ID == tostring(Existing) then
+                                  Possible = false
+                              end
+                          else
+                              if tonumber(actualHour) ~= tonumber(Existing) then
+                                  local delFile = pcall(function()
+                                      -- delfile("NotSameServers.json")
+                                      AllIDs = {}
+                                      table.insert(AllIDs, actualHour)
+                                  end)
+                              end
+                          end
+                          num = num + 1
+                      end
+                      if Possible == true then
+                          table.insert(AllIDs, ID)
+                          wait()
+                          pcall(function()
+                              -- writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                              wait()
+                              game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                          end)
+                          wait(4)
+                      end
+                  end
+              end
+          end
+
+          function Teleport()
+              while wait() do
+                  pcall(function()
+                      TPReturner()
+                      if foundAnything ~= "" then
+                          TPReturner()
+                      end
+                  end)
+              end
+          end
+
+          Teleport() 
+  	end 
+})
+
 local Section = TabSV:AddSection({
 	Name = "Anti"
 })
@@ -1738,6 +1816,33 @@ TabSV:AddToggle({
         Callback = function(AST) 		
             _G.antistun = AST	
         end })
+
+spawn(function()
+while _G.antistun do wait()
+       pcall(function()
+	if _G.antistun then
+       local khiemquadeptrai = game.Players.LocalPlayer.Character
+       repeat
+       khiemquadeptrai["DF_Disabled"].Value = false
+       khiemquadeptrai.HeartStolen.Value = true
+       khiemquadeptrai.Returned.Value = true
+       khiemquadeptrai.Hobbied.Value = true
+       khiemquadeptrai.HMS.Value = true
+       khiemquadeptrai.ChillyPunched.Value = true
+       khiemquadeptrai.CandyTouched.Value = true
+       khiemquadeptrai.Negative.Value = true
+       khiemquadeptrai.OpeSevered.Value = true
+       khiemquadeptrai.SnowTouched.Value = true
+       khiemquadeptrai.RumbleStun.Value = true
+       khiemquadeptrai.GravityCrushed.Value = true
+   
+       wait(0.06)
+       until khiemquadeptrai.Humanoid.Health == 0
+end
+end
+end)
+end 
+end)
 
 local Section = TabSV:AddSection({ 	
     Name = "Weapon" }) 
