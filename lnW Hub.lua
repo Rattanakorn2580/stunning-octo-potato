f00f3cc9dbdce65a770e1741c0d885f38be626a2
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-04 00:54:35 +0700

    Update lnW Hub.lua

--- lnW Hub.lua
+++ lnW Hub.lua
@@ -1,5 +1,4 @@
 local Ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/Akiri%20Lib/Akiri%20Lib%20Source.lua"))()
-local Ui = Library
 
 Library.CreateLoader(
     "Loader1", 
