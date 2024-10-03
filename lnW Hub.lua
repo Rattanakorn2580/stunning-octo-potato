377ac3ede5d9dfdc40b030a090093905fc0a7e43
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-04 01:50:18 +0700

    Update lnW Hub.lua

--- lnW Hub.lua
+++ lnW Hub.lua
@@ -33,44 +33,84 @@ spawn(function() -- autofarm velocity
     end
 end);
 
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cueshut/saves/main/criminality%20paste%20ui%20library'))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
 
--- // Window \\ --
-local window = library.new('lnWZa Hub', 'leadmarker')
+local Window = library:CreateWindow("lnWZa Hub", "OPL: Anarchy", 10044538000)
 
--- // Tabs \\ --
-local tab = window.new_tab('rbxassetid://4483345998')
-local tab1 = window.new_tab('rbxassetid://4483345998')
+local Tab = Window:CreateTab("Scripts")
 
--- // Sections \\ --
-local section = tab.new_section('Autos')
-local section1 = tab.new_section('Auto Farm Devil Fruit')
+local Page = Tab:CreateFrame("Autos")
 
--- // Sector \\ --
-local sector = section.new_sector('Function Auto', 'Left')
-local sector1 = section.new_sector('', 'Right')
-
--- // Elements \\ -- (Type, Name, State, Callback)
-local button = sector.element('Button', 'Button', nil, function()
-
+Button = Page:CreateButton("Button", "Description", function()
+CreateNotification("Title", "Description", function(value)
+if value == true then
+print(true)
+else
+print(false)
+end
+end)
 end)
 
-local toggle = sector.element('Auto Fishing', 'Auto Fishing', false, function(v)
-      AutoFish = v
-    end)
- 
-toggle:add_color({Color = Color3.fromRGB(84, 101, 255)}, nil, function(v)
-   print(v.Color)    
+Toggle = Page:CreateToggle("Auto Fishing", "", function(arg)
+Toggle:UpdateToggle("New Title", "New Description")
+AutoFish = arg
 end)
 
-local dropdown = sector.element('Dropdown', 'Dropdown', {options = {'one', 'two', 'three'}}, function(v)
-   print(v.Dropdown)
+ spawn(function() -- fish farm
+    while wait(0) do
+        pcall(function()
+            if AutoFish then
+                wait(0.5)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-20000, 218, 20000)
+                wait(0.5)
+                for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                    if string.find(v.Name, "Rod") then
+                        for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                            if string.find(v.Name, "Rod") then
+                                game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+                            end
+                        end
+                    end
+                end
+                for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                    if string.find(v.Name, "Rod") then
+                        for _, x in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                            if string.find(x.Name, "Rod") then
+                                for i, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                                    if v:FindFirstChild("Bobber") then
+                                        if v.Bobber.Effect.Enabled == true then
+                                            wait(0.6)
+                                            local args = {
+                                                [1] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+                                            }
+                                            game:GetService("Players").LocalPlayer.Character:FindFirstChild(x.Name).Click:FireServer(unpack(args))
+                                        end
+                                    elseif v.Name == "Cast" and not v:FindFirstChild("Bobber") then
+                                        wait(0.6)
+                                        local args = {
+                                            [1] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+                                        }
+                                        game:GetService("Players").LocalPlayer.Character:FindFirstChild(x.Name).Click:FireServer(unpack(args))
+                                        workspace:WaitForChild("Merchants"):WaitForChild("FishMerchant"):WaitForChild("Clickable"):WaitForChild("Retum"):FireServer()
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end       
+            end
+        end)
+    end
+end);
+
+TextBox = Page:CreateBox("TextBox", 10044538000, function(arg)
+TextBox:UpdateBox("New Title")
+print(arg)
 end)
 
-local slider = sector.element('Slider', 'Slider', {default = {min = 1, max = 100, default = 50}}, function(v)
-   print(v.Slider)
+Page:CreateSlider("Slider", 16, 500,function(arg)
+   print(arg)
 end)
 
-local combo = sector.element('Combo', 'Combo', {options = {'bruh', 'otherbruh'}}, function(v)
-   table.foreach(v.Combo, print)
-end)
+Label = Page:CreateLabel("Label")
+Label:UpdateLabel("New Title")
