988b26b9be4a2b62ee57ab062b62bb4d80954f69
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-04 00:58:21 +0700

    Update lnW Hub.lua

--- lnW Hub.lua
+++ lnW Hub.lua
@@ -1,1 +1,2 @@
-
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("lnW Hub", "Default")
