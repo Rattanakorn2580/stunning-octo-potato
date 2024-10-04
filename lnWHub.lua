a2c699a455d3733ec90eb051d1cb9fc1ae14b21c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-04 17:06:26 +0700

    Update lnWHub.lua

--- lnWHub.lua
+++ lnWHub.lua
@@ -5,29 +5,6 @@ local Library = DarkraiX:Window("lnWZa  X","","",Enum.KeyCode.RightControl);
 
 Tab1 = Library:Tab("Autos")
 
-spawn(function() -- autofarm velocity
-    while wait(0) do
-        pcall(function()
-            if AutoFarmM  or KillPlayer or AutoPackage or AutoFish or FarmB then
-                if not game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip") then
-                    local Noclip = Instance.new("BodyVelocity")
-                    Noclip.Name = "BodyClip"
-                    Noclip.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-                    Noclip.MaxForce = Vector3.new(100000,100000,100000)
-                    Noclip.Velocity = Vector3.new(0,0,0)
-                end
-                game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
-            elseif  AutoFarmM == false or KillPlayer == false or AutoPackage == false or AutoFish == false FarmB == false then
-                --if game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip") then
-                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip"):Destroy()
-                wait(1)
-                --end
-                game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
-            end
-        end)
-    end
-end)
-
 Tab1:Seperator("Auto Spawn")
 
 Tab1:Toggle("Auto Respawn",false,function(SP)
