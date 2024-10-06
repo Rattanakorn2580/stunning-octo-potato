039f690b4e95cf0d9ba560d76d87b60587181499
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-07 06:47:06 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -1845,10 +1845,21 @@ local Section = TabLD:AddSection({
 TabLD:AddDropdown({
 	Name = "Choose SafeZone",
 	Default = "",
-	Options = Cache.DevConfig["ListOfMerchant"],
+	Options = Cache.DevConfig["ListOfSafeZone"],
 	Callback = function(CSF)
 		getgenv().tpsafezone = CSF
 	end    
+})
+
+TabLD:AddButton({
+	Name = "Tp To SafeZone UnderSea",
+	Callback = function()
+        if getgenv().tpsafezone == "SafeZoneSky" then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace")["SafeZoneOuterSpacePart"].CFrame * CFrame.new(0, 5, 0)
+      elseif getgenv().tpsafezone == "SafeZoneDF" then
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace")["SafeZonePart"].CFrame * CFrame.new(0, 5, 0)
+			end
+			end    
 })
 
 local Section = TabLD:AddSection({
