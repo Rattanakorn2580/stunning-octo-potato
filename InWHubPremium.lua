03beaed8982761bcd296b3df04883d623b72af81
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-06 16:59:10 +0700

    Update InWHubPremium.lua

--- InWHubPremium.lua
+++ InWHubPremium.lua
@@ -6,13 +6,6 @@ local tab = win:Tab("Auto Farm", "http://www.roblox.com/asset/?id=6023426915")
 
 tab:Label("This is just a label.")
 tab:Line()
-tab:Toggle("Auto-Farm Coins", "Automatically collects coins for you!", function(t)
-print(t)
-end)
-
-tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
-print(t)
-end)
 
 local Weaponlist = {}
 local Weapon = nil
@@ -25,16 +18,8 @@ tab:Dropdown("Choose Weapon", Weaponlist,{""}, function(t)
 Weapon = t
 end)
 
-tab:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
-end)
-
-tab:Textbox("Gun Power", "This textbox changes your gun power, so you can kill everyone faster and easier.", true, function(t)
-print(t)
-end)
-
-tab:Bind("Kill Bind", Enum.KeyCode.Q, function()
-print("Killed a random person!")
+tab:Toggle("Auto Equip", "Auto Equip item u!", function(ate)
+_G.autoequip = ate
 end)
 
 win:Tab("Tab 2", "http://www.roblox.com/asset/?id=6022668888")
