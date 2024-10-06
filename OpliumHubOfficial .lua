618b3ea789dc47c2b2939a7e961214fd4a29eae6
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-07 05:11:38 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -13,6 +13,12 @@ local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
     SafeZoneOuterSpace.Position = Vector3.new((math.random(-1000000, 1000000)), (math.random(10000, 50000)), (math.random(-1000000, 1000000)))
     SafeZoneOuterSpace.Anchored = true
 
+local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
+    SafeZoneOuterSpace.Name = "SafeZoneFarmLight"
+    SafeZoneOuterSpace.Size = Vector3.new(50,3,50)
+    SafeZoneOuterSpace.Position = Vecter3.new(math.random(388, 3551, 294))
+    SafeZoneOuterSpace.Anchored = true
+
 spawn(function() -- autofarm velocity
     while wait(0) do
         pcall(function()
@@ -1745,6 +1751,7 @@ TabSk:AddButton({
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace")["SafeZoneFarmLight"].CFrame * CFrame.new(0, 5, 0)
   	end    
 })
+
 local TabPlr = Window:MakeTab({
 	Name = "Player",
 	Icon = "rbxassetid://4483345998",
