5fd721ccaecba34b52821639dbfcb8bd9d2fee2c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-06 19:15:27 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -1590,17 +1590,62 @@ TabPlr:AddToggle({
 	Name = "Behind Player",
 	Default = false,
 	Callback = function(BPR)
-		BehindPlrs = BPR
+		BehindPlr = BPR
 	end    
 })
+
+spawn(function() 	
+while wait() do
+pcall(function()	
+if BehindPlr then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[SelectPlayer].Character.HumanoidRootPart.CFrame*CFrame.new(0,0,2)
+		end
+	end)	
+	end 	
+end);
 
 TabPlr:AddToggle({
 	Name = "Auto Bring Player",
 	Default = false,
 	Callback = function(ABP)
-		AutoBringPlrs = ABP
+		BringPlr = ABP
 	end    
 })
+
+spawn(function()
+    while wait() do
+        if BringPlr then
+            pcall(function()
+                game.Players[SelectPlayer].Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame*CFrame.new(0,0,-5)
+            end)
+        end
+    end
+end);
+
+TabNPC:AddToggle({
+	Name = "Bring Player | All |",
+	Default = false,
+	Callback = function(BPA)
+		BringAll = BPA
+	end    
+})
+
+spawn(function() -- bring Plr
+    while wait() do
+        if BringAll then
+            pcall(function()
+                for i,v in pairs(game.Players:GetChildren()) do
+                    if v.Name ~= game.Players.LocalPlayer.Name then
+                        v.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame*CFrame.new(0,0,-5)
+                        if v.Character.Humanoid.Health == 0 then
+                        	v.Character.HumanoidRootPart.Size = Vector3.new(0, 0, 0)
+                        end
+                    end
+                end
+            end)
+        end
+    end
+end);
 
 local TabLand = Window:MakeTab({
 	Name = "Island",
