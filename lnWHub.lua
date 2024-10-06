6263c84b762d54753efe10c57c0adbe74041928c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-06 15:50:57 +0700

    Update lnWHub.lua

--- lnWHub.lua
+++ lnWHub.lua
@@ -2053,6 +2053,11 @@ for _, descendant in ipairs(game:GetDescendants()) do
     end
 		end
 	end)
+
+Tab1:Button("Anti Afk",function()
+    game:service'Players'.LocalPlayer.Idled:connect(function()
+end)
+
 Tab1:Seperator("Other")
 
 Tab1:Button("Seastone Cetus | 500 Melee |",function()
