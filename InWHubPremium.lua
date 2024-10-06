43146bb206505f4fce9d7bc540fbd96cd009a9b8
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-06 13:58:12 +0700

    Update InWHubPremium.lua

--- InWHubPremium.lua
+++ InWHubPremium.lua
@@ -1,2 +1,10 @@
 local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/refs/heads/main/RayfieldV1.lua"))()
 
+
+
+
+
+
+
+
+Rayfield:LoadConfiguration()
