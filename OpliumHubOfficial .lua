06a1dbdaa2a5cd7330f7c3a27d5588f08a05a5ab
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-07 05:09:05 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -13,12 +13,6 @@ local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
     SafeZoneOuterSpace.Position = Vector3.new((math.random(-1000000, 1000000)), (math.random(10000, 50000)), (math.random(-1000000, 1000000)))
     SafeZoneOuterSpace.Anchored = true
 
-local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
-    SafeZoneOuterSpace.Name = "SafeZoneFarmLight"
-    SafeZoneOuterSpace.Size = Vector3.new(50,3,50)
-    SafeZoneOuterSpace.Position = Vecter3.new(math.random(388, 3551, 294))
-    SafeZoneOuterSpace.Anchored = true
-
 spawn(function() -- autofarm velocity
     while wait(0) do
         pcall(function()
