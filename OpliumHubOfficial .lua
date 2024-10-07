fb9c5453020a66f7875b5ead0bce87aa06c8d441
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-07 22:41:36 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -2028,7 +2028,7 @@ spawn(function()
         while true do wait()
             pcall(function()
                 local plr1 = game.Players.LocalPlayer.Character
-                local plr2 = game.Players:FindFirstChild())
+                local plr2 = game.Players:FindFirstChild(SelectPlayer)
                 if aimsilent then
                     cacacac = plr2.Character.HumanoidRootPart.CFrame
                 end
