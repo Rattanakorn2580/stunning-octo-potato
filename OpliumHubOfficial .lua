b39940617d51ce76837da9920687cd5acd5d8df8
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-15 15:34:37 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -2603,14 +2603,6 @@ TabPlayer:AddToggle({
 	Default = false,
 	Callback = function(viewplr)
 		Sp = viewplr
-    local plr1 = game.Players.LocalPlayer.Character.Humanoid
-    local plr2 = game.Players:FindFirstChild(PlayerName1)
-    repeat wait(0)
-        game.Workspace.Camera.CameraSubject = plr2.Character.Humanoid
-    until Sp == false or plr2.Character.Humanoid.Health == 0
-    if Sp == false or plr2.Character.Humanoid.Health ~= 0 then
-        game.Workspace.Camera.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
-			end
 	end    
 })
 
