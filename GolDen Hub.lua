30d8acf3c1cb492218ffd1a9ca788a84144fb94e
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-17 21:59:03 +0700

    Update GolDen Hub.lua

--- GolDen Hub.lua
+++ GolDen Hub.lua
@@ -1,10 +1,46 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Golden Hub", "Synapse")
 
+local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
+    SafeZoneOuterSpace.Name = "SafeZoneOuterSpacePart"
+    SafeZoneOuterSpace.Size = Vector3.new(200,3,200)
+    SafeZoneOuterSpace.Position = Vector3.new((math.random(-1000000, 1000000)), (math.random(10000, 50000)), (math.random(-1000000, 1000000)))
+    SafeZoneOuterSpace.Anchored = true
+
+local SafeZoneFarm = Instance.new("Part",game.Workspace)
+    SafeZoneFarm.Name = "SafeZoneLightPart"
+    SafeZoneFarm.Size = Vector3.new(30,2,30)
+    SafeZoneFarm.Position = Vector3.new(-339, 3623, -641)
+    SafeZoneFarm.Anchored = true
+
+spawn(function() -- autofarm velocity
+    while wait(0) do
+        pcall(function()
+            if AutoFish or AutoPack or AutoFarmM or FarmZom then
+                if not game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip") then
+                    local Noclip = Instance.new("BodyVelocity")
+                    Noclip.Name = "BodyClip"
+                    Noclip.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+                    Noclip.MaxForce = Vector3.new(100000,100000,100000)
+                    Noclip.Velocity = Vector3.new(0,0,0)
+                end
+                game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
+            elseif  AutoFish == false or AutoPack == false or AutoFarmM == false or FarmZom == false then
+                --if game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip") then
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyClip"):Destroy()
+                wait(1)
+                --end
+                game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+            end
+        end)
+    end
+end)
+
 local Tab = Window:NewTab("Autos")
 local Section = Tab:NewSection("Auto Respawn")
 
 Section:NewToggle("Auto Respawn", "Auto Spawn", function(SPW)
+    _G.autorespawn = SPW
 end)
 
 local Section = Tab:NewSection("Auto function | All |")
