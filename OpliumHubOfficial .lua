997fda305fb2ca130bc4764706cbcfe6773aa0cf
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-17 11:58:45 +0700

    Update OpliumHubOfficial .lua

--- OpliumHubOfficial .lua
+++ OpliumHubOfficial .lua
@@ -2607,13 +2607,13 @@ TabPlayer:AddToggle({
 })
 
 local plr1 = game.Players.LocalPlayer.Character.Humanoid
-    local plr2 = game.Players:FindFirstChild(SelectPlayer)
+local plr2 = game.Players:FindFirstChild(SelectPlayer)
     repeat wait(0)
         game.Workspace.Camera.CameraSubject = plr2.Character.Humanoid
     until Sp == false or plr2.Character.Humanoid.Health == 0
     if Sp == false or plr2.Character.Humanoid.Health ~= 0 then
         game.Workspace.Camera.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
-			end	
+			end
 
 TabPlayer:AddToggle({
 	Name = "Bring Player",
