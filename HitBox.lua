dd2145cb6cc4e0f9dad4df241c75109c8c997c1c
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-25 11:34:06 +0700

    Update HitBox.lua

--- HitBox.lua
+++ HitBox.lua
@@ -135,7 +135,7 @@ Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 28.000
 Toggle.Draggable = true
 Toggle.MouseButton1Click:connect(function()
-    Library:ToggleUI()
+    Library:HitboxGui()
 end)
  
 local Corner = Instance.new("UICorner")
