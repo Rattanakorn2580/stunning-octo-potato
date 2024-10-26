6291dcb5e9d32de26fe9354a554d20f62dd1fd21
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-10-27 00:51:57 +0700

    Update ListFarmOPL.lua

--- OPL-OPA/ListFarmOPL.lua
+++ OPL-OPA/ListFarmOPL.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Taget List Mob", "DarkTheme")
+local Window = Library.CreateLib("Taget List Mob", "WhiteTheme")
 
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "ScreenGui"
