9bbd03f955c1fd3cf97fdd527655d6f035ea4a91
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-11-04 13:26:32 +0700

    Update LibRac.lua

--- LibRac.lua
+++ LibRac.lua
@@ -3640,3 +3640,8 @@ game:GetService("CoreGui").redui.MainSceen.Visible = false
 --create:Notifile("", "   Welcome " .. game.Players.LocalPlayer.Name .. " To OP:L", 5)
 
 game:GetService("CoreGui").redui.MainSceen.Visible = true
+
+local Tap1 = Window:Taps("Farming")
+local page1 = Tap1:newpage()
+
+page1:Label(" ┇ Mob Farm ┇ ")
