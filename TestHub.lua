c67ebfd7fb3a4f918b83b825d2f45fa93093a4d3
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-11-04 08:11:13 +0700

    Update TestHub.lua

--- TestHub.lua
+++ TestHub.lua
@@ -1,1 +1,31 @@
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
 
+ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+ScreenGui.IgnoreGuiInset = true
+
+TextLabel.Parent = ScreenGui
+TextLabel.Text = "Test!"
+TextLabel.Position = UDim2.new(0.5, -50, 0.5, -25)
+TextLabel.Size = UDim2.new(0, 100, 0, 50)
+TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+
+local Toggle = Instance.new("TextButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0, 90, 0, 38)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Tutorial"
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
+Toggle.TextSize = 28.000
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+ 
+local Corner = Instance.new("UICorner")
+Corner.Name = "Corner"
+Corner.Parent = Toggle
