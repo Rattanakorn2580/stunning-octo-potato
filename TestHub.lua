5a29283217c75e71315bc0ec487457055f57f7df
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-11-04 08:21:07 +0700

    Update TestHub.lua

--- TestHub.lua
+++ TestHub.lua
@@ -1,15 +1,15 @@
 local ScreenGui = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
+local LibInW = Instance.new("TextLabel")
 
 ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
 ScreenGui.IgnoreGuiInset = true
 
-TextLabel.Parent = ScreenGui
-TextLabel.Text = "Test!"
-TextLabel.Position = UDim2.new(0.5, -50, 0.5, -25)
-TextLabel.Size = UDim2.new(0, 100, 0, 50)
-TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+LibInW.Parent = ScreenGui
+LibInW.Text = ""
+LibInW.Position = UDim2.new(0.5, -50, 0.5, -25)
+LibInW.Size = UDim2.new(0, 100, 0, 50)
+LibInW.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+LibInW.TextColor3 = Color3.fromRGB(255, 255, 255)
 
 local Toggle = Instance.new("TextButton")
 Toggle.Name = "Toggle"
