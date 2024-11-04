6345e430eaf0994fdfd7bff7f7ba8974581f9211
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-11-04 08:15:32 +0700

    Update TestHub.lua

--- TestHub.lua
+++ TestHub.lua
@@ -23,7 +23,7 @@ Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
 Toggle.TextSize = 28.000
 Toggle.Draggable = true
 Toggle.MouseButton1Click:connect(function()
-    Library:ToggleUI()
+    TextLael.Visible = not TextLabel.Visible
 end)
  
 local Corner = Instance.new("UICorner")
