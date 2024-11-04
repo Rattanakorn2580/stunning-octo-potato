4944c017704996fbcc919570d1ccf88f0dbafbbd
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-11-04 12:39:57 +0700

    Update Testui.lua

--- Other-Map/Testui.lua
+++ Other-Map/Testui.lua
@@ -1,1 +1,8 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rattanakorn2580/stunning-octo-potato/refs/heads/main/LibRac.lua"))()
+local Window = create:Win("     Test Hub    ")
+game:GetService("CoreGui").redui.MainSceen.Visible = false
+
+--create:Notifile("", "   Welcome " .. game.Players.LocalPlayer.Name .. " To OP:L", 5)
+
+game:GetService("CoreGui").redui.MainSceen.Visible = true
 
