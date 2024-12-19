cb26415558d45a728d64b1baa01cc5547d83390b
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-12-19 17:43:51 +0700

    Update lnW Hub Keyless.lua

--- OPL-OPA/lnW Hub Keyless.lua
+++ OPL-OPA/lnW Hub Keyless.lua
@@ -12,7 +12,7 @@ local SafeZoneOuterSpace = Instance.new("Part",game.Workspace)
 local SafeZoneFarm = Instance.new("Part",game.Workspace)
     SafeZoneFarm.Name = "SafeZoneLightPart"
     SafeZoneFarm.Size = Vector3.new(30,2,30)
-    SafeZoneFarm.Position = Vector3.new(2350, 3623, -615)
+    SafeZoneFarm.Position = Vector3.new(2500, 3623, -615)
     SafeZoneFarm.Anchored = true
 
 spawn(function() -- autofarm velocity
