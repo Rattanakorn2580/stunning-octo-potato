ef4dc5a22e6f59c1b4c381ebee77ed4581a7f882
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2024-12-25 10:14:25 +0700

    Update lnW Hub Keyless.lua

--- OPL-OPA/lnW Hub Keyless.lua
+++ OPL-OPA/lnW Hub Keyless.lua
@@ -1,4 +1,4 @@
-ตlocal OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() 
 local Player = game.Players.LocalPlayer 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "InW Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = "กำลังโหลด . . . InW Hub"})
