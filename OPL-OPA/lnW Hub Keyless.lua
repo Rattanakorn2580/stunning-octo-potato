0a161c57b7d5b619f5dea5710f5e166e0f65b5be
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-01-02 22:01:45 +0700

    Update lnW Hub Keyless.lua

--- OPL-OPA/lnW Hub Keyless.lua
+++ OPL-OPA/lnW Hub Keyless.lua
@@ -3451,25 +3451,11 @@ end
 })
 
 TabMS:AddToggle({
-	Name = "แสปมแดชก่อกวน | เฉพาะ แบล็คลิส คนไม่ดี |",
+	Name = " ",
 	Default = false,
 	Callback = function(BKP)
 		_G.blacklist = BKP
 	end    
 })
-
-spawn(function()
-while wait() do
-pcall(function()
-if _G.blacklist then
-for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
-if string.find(v.Name, "Dash") then
-v:FireServer(CFrame.new(game.Players["T3T_XxBankKungxX"].Character.HumanoidRootPart.Position),workspace.IslandWindmill.Path)
-	end
-        end
-	end)
-	end
-	end
-end)
 
 OrionLib:Init()
