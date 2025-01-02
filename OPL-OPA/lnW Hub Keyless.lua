74d061c0a1fb3b69a7e5a247a9bc81889765ab27
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-01-02 18:45:27 +0700

    Update lnW Hub Keyless.lua

--- OPL-OPA/lnW Hub Keyless.lua
+++ OPL-OPA/lnW Hub Keyless.lua
@@ -50,31 +50,6 @@ spawn(function() -- autofarm velocity
     end
 end)
 
-    while wait() do
-	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
-if string.find(v.Name, "Dash") then
-v:FireServer(CFrame.new(game.Players["T3T_XxBankKungxX"].Character.HumanoidRootPart.Position),workspace.Water)
-end
-end
-            end
-
-    while wait() do
-	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
-if string.find(v.Name, "Shave") then
-v:FireServer(CFrame.new(game.Players["T3T_XxBankKungxX"].Character.HumanoidRootPart.Position),workspace.Water)
-end
-end
-            end
-
-    while wait() do
-	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
-if string.find(v.Name, "ShaveServer") then
-v:Destroy()
-end
-end
-            end
-
-
 local Players = game:GetService("Players")
 local BlacklistedPlayers = {
     "vQZNhF",
@@ -3496,3 +3471,27 @@ TabMS:AddToggle({
 })
 
 OrionLib:Init()
+
+while wait() do
+	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
+if string.find(v.Name, "Dash") then
+v:FireServer(CFrame.new(game.Players["T3T_XxBankKungxX"].Character.HumanoidRootPart.Position),workspace.Water)
+end
+end
+            end
+
+    while wait() do
+	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
+if string.find(v.Name, "Shave") then
+v:FireServer(CFrame.new(game.Players["T3T_XxBankKungxX"].Character.HumanoidRootPart.Position),workspace.Water)
+end
+end
+            end
+
+    while wait() do
+	for i,v in pairs(game:GetService("Workspace")["T3T_XxBankKungxX"]:GetChildren()) do
+if string.find(v.Name, "ShaveServer") then
+v:Destroy()
+end
+end
+            end
