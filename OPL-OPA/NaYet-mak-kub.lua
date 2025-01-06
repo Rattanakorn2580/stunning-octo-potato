d90612739a41099c312da314546a5118d77ad392
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-01-06 11:44:11 +0700

    Update NaYet-mak-kub.lua

--- OPL-OPA/NaYet-mak-kub.lua
+++ OPL-OPA/NaYet-mak-kub.lua
@@ -2793,13 +2793,13 @@ TabPlayer:AddToggle({
 	end    
 })
 
-spawn(function()
+spawn(function() 
     while wait() do
         if _G.chillykill then
             pcall(function()
                 for i,v in pairs(game.Players:GetChildren()) do
-                    if v.Name ~= game.Players.LocalPlayer.Name and v:FindFirstChild("Humanoid") then
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame*CFrame.new(0,-12,0)
+                    if v.Name ~= game.Players.LocalPlayer.Name then
+ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame*CFrame.new(0,-13,0)
                         if v.Character.Humanoid.Health == 0 then
                         	v.Character.HumanoidRootPart.Size = Vector3.new(0, 0, 0)
                         end
