f9e85359fd761fbbbcbd557a53572a1bb4a12d57
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-01-06 11:58:47 +0700

    Update NaYet-mak-kub.lua

--- OPL-OPA/NaYet-mak-kub.lua
+++ OPL-OPA/NaYet-mak-kub.lua
@@ -2799,7 +2799,7 @@ spawn(function()
             pcall(function()
                 for i,v in pairs(game.Players:GetChildren()) do
                     if v.Name ~= game.Players.LocalPlayer.Name then
- game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame*CFrame.new(-13,0,0)
+ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame*CFrame.new(0,-13,0)
                         if v.Character.Humanoid.Health == 0 then
                         	v.Character.HumanoidRootPart.Size = Vector3.new(0, 0, 0)
                         end
