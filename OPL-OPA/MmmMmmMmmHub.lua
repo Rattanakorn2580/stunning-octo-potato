e1fac8d18f5b6f097993d7f25c38ca629d2763e7
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-02-12 23:43:50 +0700

    Update MmmMmmMmmHub.lua

--- OPL-OPA/MmmMmmMmmHub.lua
+++ OPL-OPA/MmmMmmMmmHub.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 
 local Window = Fluent:CreateWindow({
-    Title = "Inf-Comp Hub | Only OPL: Anarchy | " .. Fluent.Version,
+    Title = "Pyramid Hub | Only OPL: Anarchy | " .. Fluent.Version,
     SubTitle = "      Made By Bankzy",
     TabWidth = 100,
     Size = UDim2.fromOffset(600, 350),
@@ -30,9 +30,6 @@ Cache.DevConfig["ListOfDropCompass"] = {"Compass"}
 
   local Tabs = {
     MainTab = Window:AddTab({ Title = "Autos", Icon = "scroll" }),
-    ShopTab = Window:AddTab({ Title = "Shop", Icon = "shopping-cart" }),
-    TeleportTab = Window:AddTab({ Title = "Teleport", Icon = "map-pin" }),
-    MiscTab = Window:AddTab({ Title = "Misc", Icon = "file-code" }),
 }
 
 Fluent:Notify({
@@ -79,30 +76,66 @@ L2.MouseButton1Click:Connect(function()
     sound:Play()
 end)
 
-local Section = Tabs.MainTab:AddSection("Weekly Comp")
+local Section = Tabs.MainTab:AddSection("Affinity Reroll At 2.0")
 
 Tabs.MainTab:AddToggle("Toggle", {
-    Title = "Auto Claim Weekly",
+    Title = "Auto Reroll Affinities at 2.0 All (Left)",
     Description = "",
     Default = false,
     Callback = function(Value)
-        _G.autoclaim = Value
+        isRunning1 = Value
     end
 })
 
-spawn(function()
-    while wait(0) do
-        pcall(function()
-            if _G.autoclaim then
-local A_1 = "Claim"
-local A_2 = "Weekly3"
-    local Event = game:GetService("Workspace").UserData["User_"..game.Players.LocalPlayer.UserId].ChallengesRemote
-    Event:FireServer(A_1,A_2)
-wait(.8)
-            end
-        end)
-    end
-end)
+if isRunning1 then
+            -- Inicia o loop se o toggle estiver ativado
+            spawn(function()
+                while isRunning1 do
+                    wait(8) -- Intervalo do loop
+                    local player = game.Players.LocalPlayer
+                    local playerId = player.UserId
+                    local userDataName = game.Workspace.UserData["User_" .. playerId]
+
+                    -- DFT1 Variables
+                    local AffMelee1 = userDataName.Data.DFT1Melee.Value
+                    local AffSniper1 = userDataName.Data.DFT1Sniper.Value
+                    local AffDefense1 = userDataName.Data.DFT1Defense.Value
+                    local AffSword1 = userDataName.Data.DFT1Sword.Value
+
+                    -- Check for DFT1
+                    if AffSniper1 == 2 and AffSword1 == 2 and AffMelee1 == 2 and AffDefense1 == 2 then
+                        script.Parent:Destroy()
+                    end
+
+                    local args1 = {
+                        [1] = "DFT1",
+                        [2] = false, -- defense
+                        [3] = false, -- melee
+                        [4] = false, -- sniper
+                        [5] = false, -- sword
+                        [6] = "Cash"
+                    }
+
+                    if AffDefense1 == 2 then
+                        args1[2] = 0 / 0
+                    end
+
+                    if AffMelee1 == 2 then
+                        args1[3] = 0 / 0
+                    end
+
+                    if AffSniper1 == 2 then
+                        args1[4] = 0 / 0
+                    end
+
+                    if AffSword1 == 2 then
+                        args1[5] = 0 / 0
+                    end
+
+                    workspace:WaitForChild("Merchants"):WaitForChild("AffinityMerchant"):WaitForChild("Clickable"):WaitForChild("Retum"):FireServer(unpack(args1))
+                end
+            end)
+end
 
 local Section = Tabs.MainTab:AddSection("Local Player")
 
@@ -116,427 +149,60 @@ workspace.UserData["User_"..game.Players.LocalPlayer.UserId].UpdateClothing_Extr
 })
 
 Tabs.MainTab:AddToggle("Toggle", {
-    Title = "Auto Reset Stats",
-    Description = "It should not be used in the main account. If your main acc status disappears, I am not responsible for anything.",
-    Default = false,
-    Callback = function(Value)
-        reset = Value
-    end
-})
-
-spawn(function() -- antistun
-    while wait(5) do
-        if reset then
-            pcall(function()
-workspace.UserData["User_"..game.Players.LocalPlayer.UserId].Stats:FireServer()
-                   end)
-            end
-        end
-end)
-
-local Section = Tabs.MainTab:AddSection("Sam Quest")
-
-Tabs.MainTab:AddButton({
-    Title = "Talk Sam",
-    Description = "",
-    Callback = function()
-        fireclickdetector(game:GetService("Workspace").Merchants.QuestMerchant.Clickable.ClickDetector)
-    end
-})
-
-Tabs.MainTab:AddToggle("Toggle", {
-    Title = "Auto Claim Compasses",
+    Title = "Auto Reroll Affinities at 2.0 All (Right)",
     Description = "",
     Default = false,
     Callback = function(Value)
-        AutoClaimCompass = Value
+        isRunning2 = Value
     end
 })
 
-spawn(function()
-    while wait() do
-        if AutoClaimCompass then
-            local args = {
-                [1] = "Claim10"
-            }
-            
-            workspace.Merchants.QuestMerchant.Clickable.Retum:FireServer(unpack(args))
-        end
-    end
-end)
+if isRunning2 then
+            -- Inicia o loop se o toggle estiver ativado
+            spawn(function()
+                while isRunning2 do
+                    wait(8) -- Intervalo do loop
+                    local player = game.Players.LocalPlayer
+                    local playerId = player.UserId
+                    local userDataName = game.Workspace.UserData["User_" .. playerId]
 
-local AutoCompass = false
+                    -- DFT2 Variables
+                    local AffMelee2 = userDataName.Data.DFT2Melee.Value
+                    local AffSniper2 = userDataName.Data.DFT2Sniper.Value
+                    local AffDefense2 = userDataName.Data.DFT2Defense.Value
+                    local AffSword2 = userDataName.Data.DFT2Sword.Value
 
-Tabs.MainTab:AddToggle("Toggle", {
-    Title = "Auto Find Compasses",
-    Description = "",
-    Default = false,
-    Callback = function(Value)
-        AutoCompass = Value
-    end
-})
-
-spawn(function()
-    while wait() do
-        if AutoCompass then
-            pcall(function()
-                local player = game.Players.LocalPlayer
-                local backpack = player.Backpack
-                local character = player.Character or player.CharacterAdded:Wait()
-                for _, item in pairs(backpack:GetChildren()) do
-                    if item.Name == "Compass" then
-                        item.Parent = character
-                        wait(0.1)
-                        if character:FindFirstChild("Compass") then
-                            local compass = character.Compass
-                            if compass:FindFirstChild("Poser") and compass.Poser.Value then
-                                character.HumanoidRootPart.CFrame = CFrame.new(compass.Poser.Value)
-                                wait(0.2) 
-                                compass:Activate()
-                                item.Parent = backpack
-                                wait(0.5)
-                            end
-                        end
+                    -- Check for DFT2
+                    if AffSniper2 == 2 and AffSword2 == 2 and AffMelee2 == 2 and AffDefense2 == 2 then
+                        script.Parent:Destroy()
                     end
+
+                    local args2 = {
+                        [1] = "DFT2",
+                        [2] = false, -- defense
+                        [3] = false, -- melee
+                        [4] = false, -- sniper
+                        [5] = false, -- sword
+                        [6] = "Cash"
+                    }
+
+		    if AffDefense2 == 2 then
+                        args2[2] = 0 / 0
+                    end
+
+                    if AffMelee2 == 2 then
+                        args2[3] = 0 / 0
+                    end
+
+                    if AffSniper2 == 2 then
+                        args2[4] = 0 / 0
+                    end
+
+                    if AffSword2 == 2 then
+                        args2[5] = 0 / 0
+                    end
+
+                    workspace:WaitForChild("Merchants"):WaitForChild("AffinityMerchant"):WaitForChild("Clickable"):WaitForChild("Retum"):FireServer(unpack(args2))
                 end
             end)
         end
-    end
-end)
-
-local DropCompass = false
-
-Tabs.MainTab:AddToggle("Toggle", {
-    Title = "Auto Drop Compasses",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        DropCompass = value 
-    end
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not DropCompass then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfDropCompass"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value.Parent = game.Workspace;
-                end
-            end
-        end)
-    end
-end);
-
-local Section = Tabs.ShopTab:AddSection("Auto Buy Drinks")
-
-Tabs.ShopTab:AddDropdown("DrinkDropdown", {
-    Title = "Choose Drink To Buy",
-    Description = "",
-    Values = {"Cider+", "Lemonade+", "Juice+", "Smoothie+"},
-    Multi = false,
-    Default = 1,
-    Callback = function(Value)
-	SelectDrink = Value
-    end
-})
-
-local Input = Tabs.ShopTab:AddInput("Input", {
-    Title = "Amount",
-    Description = "",
-    Default = "",
-    Placeholder = "Enter Amount: ",
-    Numeric = true, 
-    Finished = false, 
-    Callback = function(txt)
-        AmountDrink = txt
-    end
-})
-
-Tabs.ShopTab:AddButton({
-    Title = "Buy Drink",
-    Description = "",
-    Callback = function()
-if not AmountDrink or not string.match(AmountDrink, "%d+") or tonumber(string.match(AmountDrink, "%d+")) < 0 then return end;
-        for _ = 1, tonumber(string.match(AmountDrink, "%d+")) do
-            game.Workspace.Merchants.BetterDrinkMerchant.Clickable.Retum:FireServer(SelectDrink)
-			end
-    end
-})
-
-Tabs.ShopTab:AddToggle("Toggle", {
-    Title = "Auto Drinks",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        _G.autodrinks = value 
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not _G.autodrinks then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfDrink"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value:Activate();
-                end
-            end
-        end)
-    end
-end);
-    end
-})
-
-local AutoDropDrink = false
-
-Tabs.ShopTab:AddToggle("Toggle", {
-    Title = "Auto Drop Drinks",
-    Description = "",
-    Default = false,
-    Callback = function(Value)
-        AutoDropDrink = Value
-        spawn(function()
-    while wait() do
-        pcall(function()
-            if not AutoDropDrink then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfDrink"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value.Parent = game.Workspace;
-                end
-            end
-        end)
-    end
-end)
-    end,
-})
-
-local islandPositions = {
-    ["Cave"] = CFrame.new(-280, 217, -831),
-    ["Crab"] = CFrame.new(-7, 224, -91),
-    ["Sam"] = CFrame.new(-1283, 218, -1348),
-    ["Yellow House"] = CFrame.new(737, 241, 1209),
-    ["Bar"] = CFrame.new(1522, 260, 2188),
-    ["Krizma"] = CFrame.new(-1109, 341, 1645),
-    ["Pyramid"] = CFrame.new(118, 216, 4773),
-    ["Kaizu"] = CFrame.new(-1526.023, 365, 10510.021),
-    ["Snow Mountains"] = CFrame.new(6501, 408, -1261),
-    ["Pursuer Boss"] = CFrame.new(4847, 570, -7143),
-    ["Vokun"] = CFrame.new(4598, 217, 4964),
-    ["Green"] = CFrame.new(-2727, 253, 1041),
-    ["Trees"] = CFrame.new(1068, 217, 3351),
-    ["Merlin Fish"] = CFrame.new(-1668, 217, -300),
-    ["Snowy"] = CFrame.new(-1896, 222, 3385),
-    ["Mountain"] = CFrame.new(2052, 488, -701),
-    ["Marine Ford"] = CFrame.new(-3164, 296, -3780),
-    ["Sand Castle"] = CFrame.new(1020, 224, -3277),
-    ["Forest"] = CFrame.new(-5781, 216, 114),
-    ["Evil"] = CFrame.new(-5169, 523, -7803),
-    ["Crescent"] = CFrame.new(3193, 357, 1670),
-    ["Islands"] = CFrame.new(-4319, 245, 5252),
-    ["Town"] = CFrame.new(1818, 218, 755),
-    ["Rocky"] = CFrame.new(-37, 229, 2149),
-    ["Palm"] = CFrame.new(766, 216, -1374),
-    ["Sand"] = CFrame.new(-2747, 216, -942),
-    ["Sand 2"] = CFrame.new(162, 216, -2265),
-    ["Small"] = CFrame.new(1237, 240, -244),
-    ["Tiny"] = CFrame.new(-1235, 223, 623),
-    ["Super Tiny"] = CFrame.new(-4007, 216, -2190),
-    ["Grass"] = CFrame.new(2096, 217, -1884),
-    ["Atlar"] = game.workspace.Altar.RecepticalEffect.CFrame * CFrame.new(0, 5, 0),
-}
-
-local npcPositions = {
-    ["Rayleigh"] = game.workspace.Merchants.QuestHakiMerchant.HumanoidRootPart.CFrame,
-    ["Better Drink"] = CFrame.new(1493, 260, 2171),
-    ["Drink"] = CFrame.new(-1282, 218, -1368),
-    ["Flail"] = CFrame.new(1110, 217, 3369),
-    ["QuestFish"] = CFrame.new(-1702, 216, -325),
-    ["Krizma"] = CFrame.new(-1072, 361, 1669),
-    ["Sword"] = CFrame.new(1005, 224, -3339),
-    ["Sniper"] = CFrame.new(-1843, 222, 3416),
-    ["Emote"] = CFrame.new(1522, 265, 2165),
-    ["Affinity"] = CFrame.new(113, 278, 4952),
-    ["Fish"] = CFrame.new(1983, 218, 566),
-    ["Expertise"] = CFrame.new(903, 270, 1219),
-}
-
-local selectedIsland = nil
-local selectedNpc = nil
-
-Tabs.TeleportTab:AddDropdown("IslandDropdown", {
-    Title = "Select Island",
-    Description = "",
-    Values = {"Cave", "Windmill", "Sam", "Grassy", "Bar", "Krizma", "Kaizu", "Snow Mountains", "Pursuer Boss", "Cliffs", "Green", "Trees", "Pyramid", "Merlin Fish", "Snowy", "Mountain", "Marine Ford", "Sand Castle", "Forest", "Evil", "Crescent", "Islands", "Town", "Rocky", "Palm", "Sand", "Sand 2", "Small", "Tiny", "Super Tiny", "Grass", "Atlar"},
-    Multi = false,
-    Default = 1,
-    Callback = function(Value)
-        selectedIsland = Value
-    end
-})
-
-Tabs.TeleportTab:AddButton({
-    Title = "Teleport to Island",
-    Description = "",
-    Callback = function()
-        if selectedIsland then
-            local islandPosition = islandPositions[selectedIsland]
-            if islandPosition then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = islandPosition
-            else
-            end
-        else
-        end
-    end
-})
-
-local Section = Tabs.TeleportTab:AddSection("Npc TP")
-
-Tabs.TeleportTab:AddDropdown("NpcDropdown", {
-    Title = "Select NPC",
-    Description = "",
-    Values = {"Rayleigh", "Better Drink", "Drink", "Flail", "QuestFish", "Krizma", "Sword", "Sniper", "Emote", "Affinity", "Fish", "Expertise"},
-    Multi = false,
-    Default = 1,
-    Callback = function(Value)
-        selectedNpc = Value
-    end
-})
-
-Tabs.TeleportTab:AddButton({
-    Title = "Teleport to NPC",
-    Description = "",
-    Callback = function()
-        if selectedNpc then
-            local npcPosition = npcPositions[selectedNpc]
-            if npcPosition then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = npcPosition
-            else
-            end
-        else
-        end
-    end
-})
-
-local Section = Tabs.MiscTab:AddSection("Function Server")
-
-Tabs.MiscTab:AddButton({
-    Title = "Rejoin",
-    Description = "",
-    Callback = function()
-        local TeleportService = game:GetService("TeleportService")
-        local Players = game:GetService("Players")
-        local LocalPlayer = Players.LocalPlayer
-
-        local Success, ErrorMessage = pcall(function()
-            TeleportService:Teleport(game.PlaceId, LocalPlayer)
-        end)
-
-        if ErrorMessage and not Success then
-            warn(ErrorMessage)
-        end
-    end
-})
-
-local Section = Tabs.MiscTab:AddSection("Unbox")
-
-Tabs.MiscTab:AddToggle("Toggle", {
-    Title = "Auto Unbox (Common)",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        UnboxCM = value 
-    end
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not UnboxCM then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfBox1"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value:Activate();
-                end
-            end
-        end)
-    end
-end);
-
-Tabs.MiscTab:AddToggle("Toggle", {
-    Title = "Auto Unbox (Uncom)",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        UnboxUn = value 
-    end
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not UnboxUn then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfBox2"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value:Activate();
-                end
-            end
-        end)
-    end
-end);
-
-Tabs.MiscTab:AddToggle("Toggle", {
-    Title = "Auto Unbox (Rare,Ultra)",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        UnboxRUL = value 
-    end
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not UnboxRUL then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfBox3"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value:Activate();
-                end
-            end
-        end)
-    end
-end);
-
-local Section = Tabs.MiscTab:AddSection("Drop Box")
-
-Tabs.MiscTab:AddToggle("Toggle", {
-    Title = "Auto Drop (Rare,Ultra)",
-    Description = "",
-    Default = false, 
-    Callback = function(value)
-        DropCompass = value 
-    end
-})
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if not DropCompass then return end;
-            for _, Value in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                if table.find(Cache.DevConfig["ListOfBox3"], Value.Name) then
-                    game.Players.LocalPlayer.Character.Humanoid:UnequipTools();
-                    Value.Parent = game.Players.LocalPlayer.Character;
-                    Value.Parent = game.Workspace;
-                end
-            end
-        end)
-    end
-end);
