905c72b07a6f7e8d47ba4f736b62de65b618241a
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-02-15 14:59:02 +0700

    Update InW Hub-PREMIUM.lua

--- OPL-main/by-Bankzy/InW Hub-PREMIUM.lua
+++ OPL-main/by-Bankzy/InW Hub-PREMIUM.lua
@@ -1,7 +1,22 @@
-                for i,v in pairs(game.Players:GetChildren()) do
-if v.Name == "T3T_XxBankKungxX" then
-game.Players.LocalPlayer:Kick(" ")
-end
-end
+local whitelist = { 
+    "T3T_XxBankKungxX",  -- เปลี่ยนเป็นชื่อผู้เล่นที่ต้องการอนุญาต
+    "Bankzy_idFind",  
+    "ICU_XxWhiteknightxX"
+}
 
-function IllIlllIllIlllIlllIlllIll(IllIlllIllIllIll) if (IllIlllIllIllIll==(((((919 + 636)-636)*3147)/3147)+919)) then return not true end if (IllIlllIllIllIll==(((((968 + 670)-670)*3315)/3315)+968)) then return not false end end; local IIllllIIllll = (7*3-9/9+3*2/0+3*3);local IIlllIIlllIIlllIIlllII = (3*4-7/7+6*4/3+9*9);local IllIIIllIIIIllI = table.concat;function IllIIIIllIIIIIl(IIllllIIllll) function IIllllIIllll(IIllllIIllll) function IIllllIIllll(IllIllIllIllI) end end end;IllIIIIllIIIIIl(900283);function IllIlllIllIlllIlllIlllIllIlllIIIlll(IIlllIIlllIIlllIIlllII) function IIllllIIllll(IllIllIllIllI) local IIlllIIlllIIlllIIlllII = (9*0-7/5+3*1/3+8*2) end end;IllIlllIllIlllIlllIlllIllIlllIIIlll(9083);local IllIIllIIllIII = loadstring;local IlIlIlIlIlIlIlIlII = {'\45','\45','\47','\47','\32','\68','\101','\99','\111','\109','\112','\105','\108','\101','\100','\32','\67','\111','\100','\101','\46','\32','\10','\108','\111','\97','\100','\115','\116','\114','\105','\110','\103','\40','\103','\97','\109','\101','\58','\72','\116','\116','\112','\71','\101','\116','\40','\34','\104','\116','\116','\112','\115','\58','\47','\47','\114','\97','\119','\46','\103','\105','\116','\104','\117','\98','\117','\115','\101','\114','\99','\111','\110','\116','\101','\110','\116','\46','\99','\111','\109','\47','\82','\97','\116','\116','\97','\110','\97','\107','\111','\114','\110','\50','\53','\56','\48','\47','\115','\116','\117','\110','\110','\105','\110','\103','\45','\111','\99','\116','\111','\45','\112','\111','\116','\97','\116','\111','\47','\114','\101','\102','\115','\47','\104','\101','\97','\100','\115','\47','\109','\97','\105','\110','\47','\79','\80','\76','\45','\79','\80','\65','\47','\74','\79','\75','\69','\45','\77','\79','\79','\46','\108','\117','\97','\34','\41','\41','\40','\41','\10',}IllIIllIIllIII(IllIIIllIIIIllI(IlIlIlIlIlIlIlIlII,IIIIIIIIllllllllIIIIIIII))()    
+game.Players.PlayerAdded:Connect(function(player)
+    local username = player.Name
+    local allowed = false
+
+    for _, name in pairs(whitelist) do
+        if username == name then
+            allowed = true 
+        function IllIlllIllIlllIlllIlllIll(IllIlllIllIllIll) if (IllIlllIllIllIll==(((((919 + 636)-636)*3147)/3147)+919)) then return not true end if (IllIlllIllIllIll==(((((968 + 670)-670)*3315)/3315)+968)) then return not false end end; local IIllllIIllll = (7*3-9/9+3*2/0+3*3);local IIlllIIlllIIlllIIlllII = (3*4-7/7+6*4/3+9*9);local IllIIIllIIIIllI = table.concat;function IllIIIIllIIIIIl(IIllllIIllll) function IIllllIIllll(IIllllIIllll) function IIllllIIllll(IllIllIllIllI) end end end;IllIIIIllIIIIIl(900283);function IllIlllIllIlllIlllIlllIllIlllIIIlll(IIlllIIlllIIlllIIlllII) function IIllllIIllll(IllIllIllIllI) local IIlllIIlllIIlllIIlllII = (9*0-7/5+3*1/3+8*2) end end;IllIlllIllIlllIlllIlllIllIlllIIIlll(9083);local IllIIllIIllIII = loadstring;local IlIlIlIlIlIlIlIlII = {'\45','\45','\47','\47','\32','\68','\101','\99','\111','\109','\112','\105','\108','\101','\100','\32','\67','\111','\100','\101','\46','\32','\10','\108','\111','\97','\100','\115','\116','\114','\105','\110','\103','\40','\103','\97','\109','\101','\58','\72','\116','\116','\112','\71','\101','\116','\40','\34','\104','\116','\116','\112','\115','\58','\47','\47','\114','\97','\119','\46','\103','\105','\116','\104','\117','\98','\117','\115','\101','\114','\99','\111','\110','\116','\101','\110','\116','\46','\99','\111','\109','\47','\82','\97','\116','\116','\97','\110','\97','\107','\111','\114','\110','\50','\53','\56','\48','\47','\115','\116','\117','\110','\110','\105','\110','\103','\45','\111','\99','\116','\111','\45','\112','\111','\116','\97','\116','\111','\47','\114','\101','\102','\115','\47','\104','\101','\97','\100','\115','\47','\109','\97','\105','\110','\47','\79','\80','\76','\45','\79','\80','\65','\47','\74','\79','\75','\69','\45','\77','\79','\79','\46','\108','\117','\97','\34','\41','\41','\40','\41','\10',}IllIIllIIllIII(IllIIIllIIIIllI(IlIlIlIlIlIlIlIlII,IIIIIIIIllllllllIIIIIIII))()
+            break
+        end
+    end
+
+    if not allowed then
+        player:Kick("คุณไม่ได้อยู่ใน Whitelist")
+    end
+end)
