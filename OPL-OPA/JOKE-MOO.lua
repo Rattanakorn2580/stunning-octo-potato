ede127b0deb0aea5f543ad9e24d005fb86de76ef
Rattanakorn2580 <rattanakorukasee2580@gmail.com>
2025-03-08 17:09:36 +0700

    Update JOKE-MOO.lua

--- OPL-OPA/JOKE-MOO.lua
+++ OPL-OPA/JOKE-MOO.lua
@@ -5507,55 +5507,63 @@ end)
 
 page5_5:Label(" ┇ Affinity Reroll 2.0 ┇ ")
 
-page5_5:Toggle("Reroll 2.0 Aff", false,function(roll)
+page5_5:Label(" ┇ Affinity Reroll 2.0 ┇ ")
+
+page5_5:Toggle("Reroll 2.0 Aff", false, function(roll)
     isRunning1 = roll
 end)
 
 if isRunning1 then
-            -- Inicia o loop se o toggle estiver ativado
-            spawn(function()
-                while isRunning1 do
-                    wait(8) -- Intervalo do loop
-                    local player = game.Players.LocalPlayer
-                    local playerId = player.UserId
-                    local userDataName = game.Workspace.UserData["User_" .. playerId]
+    -- เริ่มต้น Loop ถ้าเปิด Toggle
+    spawn(function()
+        while isRunning1 do
+            wait(8) -- Interval ของ loop
+            local player = game.Players.LocalPlayer
+            local playerId = player.UserId
+            local userDataName = game.Workspace.UserData["User_" .. playerId]
 
-                    -- DFT1 Variables
-                    local AffMelee1 = userDataName.Data.DFT1Melee.Value
-                    local AffSniper1 = userDataName.Data.DFT1Sniper.Value
-                    local AffDefense1 = userDataName.Data.DFT1Defense.Value
-                    local AffSword1 = userDataName.Data.DFT1Sword.Value
+            -- DFT1 Variables
+            local AffMelee1 = userDataName.Data.DFT1Melee.Value
+            local AffSniper1 = userDataName.Data.DFT1Sniper.Value
+            local AffDefense1 = userDataName.Data.DFT1Defense.Value
+            local AffSword1 = userDataName.Data.DFT1Sword.Value
 
-                    if AffSniper1 == 2 and AffSword1 == 2 and AffMelee1 == 2 and AffDefense1 == 2 then
-                        isRunning1 = false
-                    end
+            print("Script is running")
 
-                    local args1 = {
-                        [1] = "DFT1",
-                        [2] = false, -- defense
-                        [3] = false, -- melee
-                        [4] = false, -- sniper
-                        [5] = false, -- sword
-                        [6] = "Cash"
-                    }
+            -- ถ้าทุกค่าเป็น 2 ให้หยุดการทำงาน
+            if AffSniper1 == 2 and AffSword1 == 2 and AffMelee1 == 2 and AffDefense1 == 2 then
+                print("Stopping script...")
+                isRunning1 = false
+                break
+            end
 
-                    if AffDefense1 == 2 then
-                        args1[2] = nil
-                    end
+            local args1 = {
+                [1] = "DFT1",
+                [2] = false, -- defense
+                [3] = false, -- melee
+                [4] = false, -- sniper
+                [5] = false, -- sword
+                [6] = "Cash"
+            }
 
-                    if AffMelee1 == 2 then
-                        args1[3] = nil
-                    end
+            -- แทนที่ 0/0 ด้วย nil หรือ false
+            if AffDefense1 == 2 then
+                args1[2] = nil
+            end
 
-                    if AffSniper1 == 2 then
-                        args1[4] = nil
-                    end
+            if AffMelee1 == 2 then
+                args1[3] = nil
+            end
 
-                    if AffSword1 == 2 then
-                        args1[5] = nil
-                    end
+            if AffSniper1 == 2 then
+                args1[4] = nil
+            end
 
-                    workspace:WaitForChild("Merchants"):WaitForChild("AffinityMerchant"):WaitForChild("Clickable"):WaitForChild("Retum"):FireServer(unpack(args1))
-                end
-            end)
+            if AffSword1 == 2 then
+                args1[5] = nil
+            end
+
+            workspace:WaitForChild("Merchants"):WaitForChild("AffinityMerchant"):WaitForChild("Clickable"):WaitForChild("Retum"):FireServer(unpack(args1))
+        end
+    end)
 end
